b914a3c571a645721a3115c770ed88ac4a46c484
Austin Henriksen <austin@zeroc.com>
2021-02-08 16:58:47 -0500

    First working copy. Needs cleanup though.

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,20 @@
 
 mod parser;
 mod definitions;
+mod visitor;
 mod cs_generator;
 
-use std::fs;
+use visitor::Visitor;
+use std::io::Write;
+use std::fs::{read_to_string, File};
 
 fn main() {
-    let slice_file = fs::read_to_string("test.ice").expect("Wheres the file?!");
+    let slice_file = read_to_string("test.ice").expect("Wheres the file?!");
     let tree = parser::parse(&slice_file).unwrap();
-    //cs_generator::generate_from(tree);
+
+    let mut generator = cs_generator::CsGenerator { output: "".to_owned() };
+    generator.generate_from(&tree);
+
+    let mut output_file = File::create("result.cs").expect("Unable to create file!!");
+    output_file.write_all(generator.output.as_bytes()).expect("Failed to write!!");
 }
--- src/parser.rs
+++ src/parser.rs
@@ -2,83 +2,107 @@
 use crate::definitions::*;
 
 extern crate pest_consume;
-use pest_consume::Error;
-use pest_consume::Parser;
+use pest_consume::{match_nodes, Error, Parser};
 
 type ParseResult<T> = std::result::Result<T, Error<Rule>>;
 type ParseNode<'a> = pest_consume::Node<'a, Rule, ()>;
+
+
 
 #[derive(Parser)]
 #[grammar = "slice.pest"]
 pub struct SliceParser;
 
+
+
 #[pest_consume::parser]
 impl SliceParser {
     fn main(input: ParseNode) -> ParseResult<Vec<Module>> {
-        Ok(())
+        Ok(match_nodes!(input.into_children();
+            [module_def(modules).., EOI(_)] => { modules.collect() }
+        ))
     }
 
-    fn definitions(input: ParseNode) -> ParseResult<()> {
-        Ok(())
+    fn definition(input: ParseNode) -> ParseResult<Box<dyn Definition>> {
+        Ok(match_nodes!(input.into_children();
+            [module_def(module_def)]       => { Box::new(module_def) },
+            [struct_def(struct_def)]       => { Box::new(struct_def) },
+            [interface_def(interface_def)] => { Box::new(interface_def) }
+        ))
     }
 
-    fn module_def(input: ParseNode) -> ParseResult<()> {
-        Ok(())
+    fn module_def(input: ParseNode) -> ParseResult<Module> {
+        Ok(match_nodes!(input.into_children();
+            [_, identifier(identifier), definition(definitions)..] => {
+                Module {
+                    identifier: identifier,
+                    content: definitions.collect(),
+                }
+            }
+        ))
     }
 
-    fn struct_def(input: ParseNode) -> ParseResult<()> {
-        Ok(())
+    fn struct_def(input: ParseNode) -> ParseResult<Struct> {
+        Ok(match_nodes!(input.into_children();
+            [_, identifier(identifier), data_member(members)..] => {
+                Struct {
+                    identifier: identifier,
+                    content: members.collect(),
+                }
+            }
+        ))
     }
 
-    fn interface_def(input: ParseNode) -> ParseResult<()> {
-        Ok(())
+    fn interface_def(input: ParseNode) -> ParseResult<Interface> {
+        Ok(match_nodes!(input.into_children();
+            [_, identifier(identifier)] => {
+                Interface {
+                    identifier: identifier,
+                }
+            },
+        ))
     }
 
-    fn data_member(input: ParseNode) -> ParseResult<()> {
-        Ok(())
+    fn data_member(input: ParseNode) -> ParseResult<DataMember> {
+        Ok(match_nodes!(input.into_children();
+            [typename(typename), identifier(identifier)] => {
+                DataMember {
+                    typename: typename,
+                    identifier: identifier,
+                }
+            },
+        ))
     }
 
-    fn identifier(input: ParseNode) -> ParseResult<()> {
-        Ok(())
+    fn typename(input: ParseNode) -> ParseResult<Type> {
+        Ok(input.as_str().parse::<Type>().unwrap())
     }
 
-    fn scoped_identifier(input: ParseNode) -> ParseResult<()> {
-        Ok(())
+    fn identifier(input: ParseNode) -> ParseResult<String> {
+        Ok(input.as_str().to_owned())
     }
 
-    fn typename(input: ParseNode) -> ParseResult<()> {
-        Ok(())
+    fn scoped_identifier(input: ParseNode) -> ParseResult<String> {
+        Ok(input.as_str().to_owned())
     }
 
-    fn builtin_type(input: ParseNode) -> ParseResult<()> {
-        Ok(())
-    }
+    fn module_kw(input:ParseNode) -> ParseResult<()> { Ok(()) }
+    fn struct_kw(input:ParseNode) -> ParseResult<()> { Ok(()) }
+    fn interface_kw(input:ParseNode) -> ParseResult<()> { Ok(()) }
+    fn int_kw(input:ParseNode) -> ParseResult<()> { Ok(()) }
+    fn string_kw(input:ParseNode) -> ParseResult<()> { Ok(()) }
 
-    fn module_kw(input: ParseNode) -> ParseResult<()> {
-        Ok(())
-    }
-
-    fn struct_kw(input: ParseNode) -> ParseResult<()> {
-        Ok(())
-    }
-
-    fn interface_kw(input: ParseNode) -> ParseResult<()> {
-        Ok(())
-    }
-
-    fn int_kw(input: ParseNode) -> ParseResult<()> {
-        Ok(())
-    }
-
-    fn string_kw(input: ParseNode) -> ParseResult<()> {
+    fn EOI(input: ParseNode) -> ParseResult<()> {
         Ok(())
     }
 }
 
+
+
 pub fn parse(input: &String) -> Result<Vec<Module>, Error<Rule>>
 {
     let nodes = SliceParser::parse(Rule::main, &input)?;
-    let root_node = nodes.single()?;
+    let root = nodes.single()?;
 
-    SliceParser::main(root_node)
+    SliceParser::main(root)
 }
