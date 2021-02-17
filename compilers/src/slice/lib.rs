36403b5c95eb717c9a089dd5a061bd0316b7cb14
Austin Henriksen <austin@zeroc.com>
2021-02-17 18:43:27 -0500

    TEMP: stopping for the day.

--- compilers/src/slice/lib.rs
+++ compilers/src/slice/lib.rs
@@ -1,20 +1,35 @@
 
-pub mod grammar;
-pub mod options;
-pub mod parser;
-pub mod util;
-pub mod visitor;
-pub mod writer;
-mod consumer;
-mod tree_patcher;
-
-use options::SliceOptions;
-use parser::{SliceAst, SliceFile};
-use util::SliceError;
-
-pub type ParseResult = Result<(SliceAst, Vec<SliceFile>), SliceError>;
-
-pub fn parse(opts: &SliceOptions) -> ParseResult {
-    // we parse everything here and combine them into one big tree
-    unimplemented!() //TODO
+//------------------------------------------------------------------------------
+// Node
+//------------------------------------------------------------------------------
+/// Base trait that all grammar elements implement.
+trait Node {
+    fn visit(&self, visitor: &mut impl Visitor, ast: &SliceAst);
+    fn get_kind(&self) -> NodeKind;
+    fn get_location(&self) -> Location;
 }
+
+//------------------------------------------------------------------------------
+// NodeKind
+//------------------------------------------------------------------------------
+/// Enum defining all the different kinds of grammar elements.
+#[derive(Clone, Eq, Hash, PartialEq, Debug)]
+enum NodeKind {
+    Module,
+    Struct,
+    Interface,
+    DataMember,
+    Identifier,
+    TypeUse,
+}
+
+//------------------------------------------------------------------------------
+// Definition
+//------------------------------------------------------------------------------
+/// Base trait that all slice definitions implement.
+/// All Definitions are stored by index in the AST vector.
+trait Definition: Node {
+    fn get_id(&self) -> usize;
+    fn set_id(&mut self, id: usize);
+}
+
