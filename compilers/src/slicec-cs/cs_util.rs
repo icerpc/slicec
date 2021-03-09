ebef12236484a6d4778fbacedfa126023d107479
Austin Henriksen <austin@zeroc.com>
2021-03-09 14:36:48 -0500

    Forgot what scopes were like in C#.

--- compilers/src/slicec-cs/cs_util.rs
+++ compilers/src/slicec-cs/cs_util.rs
@@ -6,11 +6,11 @@ pub fn type_to_string(node: &Node) -> String {
     match node {
         Node::Struct(_, struct_def) => {
             let mut identifier = struct_def.scope.as_ref().unwrap().clone() + "::" + struct_def.identifier();
-            identifier.drain(2..).collect()
+            identifier.drain(2..).collect::<String>().replace("::", ".")
         },
         Node::Interface(_, interface_def) => {
             let mut identifier = interface_def.scope.as_ref().unwrap().clone() + "::" + interface_def.identifier();
-            identifier.drain(2..).collect()
+            identifier.drain(2..).collect::<String>().replace("::", ".")
         },
         Node::Builtin(_, builtin) => {
             match builtin {
