873c1bb680f2175178d0cf025b2b269bc0b765bc
Austin Henriksen <austin@zeroc.com>
2021-10-13 16:49:36 -0400

    Added support for passing directories on the command line.

--- slicec/src/parser.rs
+++ slicec/src/parser.rs
@@ -12,6 +12,8 @@ use pest_consume::{match_nodes, Error as PestError, Parser as PestParser};
 use std::cell::RefCell;
 use std::collections::HashMap;
 use std::fs;
+use std::io;
+use std::path::PathBuf;
 
 type PestResult<T> = Result<T, PestError<Rule>>;
 type PestNode<'a, 'b> = pest_consume::Node<'a, Rule, &'b RefCell<ParserData>>;
@@ -23,6 +25,49 @@ fn from_span(input: &PestNode) -> Location {
         end: span.end_pos().line_col(),
         file: input.user_data().borrow().current_file.clone(),
     }
+}
+
+fn find_slice_files(paths: &Vec<String>) -> Vec<String> {
+    let mut slice_paths = Vec::new();
+    for path in paths {
+        match find_slice_files_in_path(PathBuf::from(path)) {
+            Ok(child_paths) => slice_paths.extend(child_paths),
+            Err(err) => eprintln!("failed to read file '{}': {}", path, err),
+        }
+    }
+
+    let mut string_paths = slice_paths.into_iter()
+        .map(|path| path.to_str().unwrap().to_owned())
+        .collect::<Vec<_>>();
+
+    string_paths.sort();
+    string_paths.dedup();
+    string_paths
+}
+
+fn find_slice_files_in_path(path: PathBuf) -> io::Result<Vec<PathBuf>> {
+    // If the path is a directory, recursively search it for more slice files.
+    if fs::metadata(&path)?.is_dir() {
+        find_slice_files_in_directory(path.read_dir()?)
+    } else
+    // If the path is not a directory, check if it ends with 'ice'.
+    if path.extension().filter(|ext| ext.to_str() == Some("ice")).is_some() {
+        Ok(vec![path])
+    } else {
+        Ok(vec![])
+    }
+}
+
+fn find_slice_files_in_directory(dir: fs::ReadDir) -> io::Result<Vec<PathBuf>> {
+    let mut paths = Vec::new();
+    for child in dir {
+        let child_path = child?.path();
+        match find_slice_files_in_path(child_path.clone()) {
+            Ok(child_paths) => paths.extend(child_paths),
+            Err(err) => eprintln!("failed to read file '{}': {}", child_path.display(), err),
+        }
+    }
+    Ok(paths)
 }
 
 #[derive(Debug, Default)]
@@ -52,11 +97,11 @@ impl SliceParser {
     ) -> (Ast, HashMap<String, SliceFile>, ErrorHandler) {
         let mut parser = SliceParser::new();
 
-        for path in options.sources.iter() {
-            parser.try_parse_file(path, true);
+        for path in find_slice_files(&options.sources) {
+            parser.try_parse_file(&path, true);
         }
-        for path in options.references.iter() {
-            parser.try_parse_file(path, false);
+        for path in find_slice_files(&options.references) {
+            parser.try_parse_file(&path, false);
         }
 
         let data = parser.user_data.into_inner();
