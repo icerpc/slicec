ff6946ccc06c5bfd2bdffe907d1e39b3ae4f4f83
Joe George <joe@zeroc.com>
2021-10-15 15:55:19 -0400

    Fix traits/mod.rs

--- slicec-cs/src/traits/mod.rs
+++ slicec-cs/src/traits/mod.rs
@@ -7,7 +7,7 @@ mod cs_operation;
 mod cs_primitive;
 mod cs_typeref;
 
-pub use cs_interface::CsInterface;
+pub use cs_interface::CsInterfaceInfo;
 pub use cs_member::{CsMemberInfo, MemberListInfo};
 pub use cs_named_symbol::CsNamedSymbol;
 pub use cs_operation::CsOperation;
