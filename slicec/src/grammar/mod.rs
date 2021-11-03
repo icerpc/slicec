c249aa92500a42abeea1607416c6dd890e644a06
Austin Henriksen <austin@zeroc.com>
2021-11-03 16:18:10 -0400

    Qualified grammar module re-exports to avoid ambiguity.

--- slicec/src/grammar/mod.rs
+++ slicec/src/grammar/mod.rs
@@ -8,8 +8,8 @@ mod wrappers;
 
 // Re-export the contents of the grammar submodules directly into the grammar module. This is
 // for convenience, so users don't need to worry about the submodule structure while importing.
-pub use comments::*;
-pub use slice::*;
-pub use traits::*;
-pub use util::*;
-pub use wrappers::*;
+pub use self::comments::*;
+pub use self::slice::*;
+pub use self::traits::*;
+pub use self::util::*;
+pub use self::wrappers::*;
