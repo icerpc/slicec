3f1de9726022dbd8cbde8e846a1e0dcd49d57886
Reece Humphreys <reecewh@icloud.com>
2022-04-21 16:34:52 -0400

    Added support_types_do_not_produce_errors test

--- tests/encoding.rs
+++ tests/encoding.rs
@@ -140,6 +140,40 @@ enum E : int32 {}");
         use std::collections::HashMap;
 
         #[test]
+        fn support_types_do_not_produce_errors() {
+            // Test setup
+            let type_cases = HashMap::from([
+                ("2", vec!["bool", "int8", "uint8", "int16", "uint16", "int32", "uint32", "varint32", "varuint32",
+                        "int64", "uint64", "varint62", "varuint62", "float32", "float64", "string"]),
+                ("1", vec!["bool", "uint8", "int16", "int32", "int64", "float32","float64", "string", "AnyClass"])
+                ]);
+            for (encoding, value) in type_cases.iter().flat_map(|(encoding, value)| value.iter().map(move |v| (encoding, v))) {
+                supported_types(encoding, value)
+            }
+
+            fn supported_types(encoding: &str, value: &str) {
+                // Arrange
+                let slice = &format!("
+                    encoding = {encoding};
+                    module Test;
+                    compact struct S
+                    {{
+                        v: {value},
+                    }}",
+                encoding = encoding,
+                value = value,
+                );
+
+                // Act
+                let error_reporter = parse(slice);
+
+                // Assert
+                assert!(!error_reporter.has_errors(true));
+            }
+        }
+
+
+        #[test]
         fn unsupported_types_produce_error() {
             // Test setup
             let type_cases = HashMap::from([
