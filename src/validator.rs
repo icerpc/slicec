33424604dd855588f8f5a7a8794d9ad108646a5a
Austin Henriksen <austin@zeroc.com>
2022-04-25 11:51:53 -0400

    Streamed parameters must be the last parameter in an operation.

--- src/validator.rs
+++ src/validator.rs
@@ -16,7 +16,7 @@ impl<'a> Visitor for Validator<'a> {
             // Compact structs cannot be empty.
             if struct_def.members().is_empty() {
                 self.error_reporter.report_error(
-                    "compact structs cannot be empty",
+                    "compact structs cannot be empty".to_owned(),
                     Some(&struct_def.location),
                 )
             } else {
@@ -41,5 +41,26 @@ impl<'a> Visitor for Validator<'a> {
                 }
             }
         }
+    }
+
+    fn visit_operation_start(&mut self, operation_def: &Operation) {
+        fn validate_stream_member(error_reporter: &mut ErrorReporter, members: Vec<&Parameter>) {
+            // If members is empty, `split_last` returns None, and this check is skipped,
+            // otherwise it returns all the members, except for the last one. None of these members
+            // can be streamed, since only the last member can be.
+            if let Some((_, unstreamable_members)) = members.split_last() {
+                for member in unstreamable_members {
+                    if member.is_streamed {
+                        error_reporter.report_error(
+                            "only the last parameter in an operation can be streamed".to_owned(),
+                            Some(&member.location),
+                        );
+                    }
+                }
+            }
+        }
+
+        validate_stream_member(self.error_reporter, operation_def.parameters());
+        validate_stream_member(self.error_reporter, operation_def.return_members());
     }
 }
