2cf7b0b8622a40da12f6407a4148350ae86bef95
Reece Humphreys <reecewh@icloud.com>
2022-04-28 10:57:00 -0400

    Slice 2 dictionary key types test (#117)

---
+++ tests/dictionaries/container.rs
@@ -0,0 +1,171 @@
+// Copyright (c) ZeroC, Inc. All rights reserved.
+
+use crate::helpers::parsing_helpers::parse_for_errors;
+
+mod slice1 {}
+
+mod slice2 {
+
+    use super::*;
+
+    /// Invalid primitive dictionary key types test.
+    macro_rules! test_primitive_dictionary_key_types_fails {
+        ($(($test_name:ident, $key_type:expr)),*) => {
+            $(
+                #[test]
+                #[ignore] // Remove ignore when disallowed key errors are added.
+                fn $test_name() {
+                    // Arrange
+                    let slice = &format!(
+                        "
+                        encoding = 2;
+                        module Test;
+                        typealias TestDict = dictionary<{key_type}, int32>;
+                        ",
+                        key_type=$key_type
+                    );
+                    let expected_errors = &[&format!(
+                        "{} cannot be used as a dictionary key type",
+                        $key_type,
+                    ),
+                    "'TestDict' was defined here:",
+                    ]; // Add the expected errors here when disallowed key errors are added.
+
+                    // Act
+                    let error_reporter = parse_for_errors(slice);
+
+                    // Assert
+                    error_reporter.assert_errors(expected_errors);
+                }
+            )*
+        }
+    }
+
+    // Invalid primitive dictionary key types test cases.
+    test_primitive_dictionary_key_types_fails!(
+        (optional_int32_as_key_invalid, "int32?"),
+        (optional_proxy_as_key_invalid, "proxy"),
+        (any_class_as_key_invalid, "AnyClass")
+    );
+
+    /// Invalid Constructed dictionary key types test.
+    macro_rules! test_constructed_dictionary_key_types_fails {
+        ($(($test_name:ident, $key_type:expr, $key_ident:expr, $key_def:expr)),*) => {
+            $(
+                #[test]
+                #[ignore] // Remove ignore when disallowed key errors are added.
+                fn $test_name() {
+                    // Arrange
+                    let slice = &format!("
+                        encoding = 2;
+                        module test;
+
+                        {key_def}
+
+                        typealias TestDict = dictionary<{key_type}, int32>;
+                        ",
+                        key_def = $key_def,
+                        key_type = $key_ident,
+                    );
+
+                    let expected_errors: [&str; 2] = [
+                        &format!(
+                            "{} '{}' cannot be used as a dictionary key type",
+                            $key_type,
+                            $key_ident,
+                        ),
+                        &format!(
+                            "{} was defined here:",
+                            $key_ident,
+                        ),
+                    ];
+                    // Act
+                    let error_reporter = parse_for_errors(slice);
+
+                    // Assert
+                    error_reporter.assert_errors(&expected_errors);
+                }
+            )*
+        }
+    }
+
+    // Invalid Constructed dictionary key types test cases.
+    test_constructed_dictionary_key_types_fails!(
+        (
+            dictionary_as_key_invalid,
+            "dictionary",
+            "MyDict",
+            "typealias MyDict = dictionary<int32, int32>"
+        ),
+        (proxy_as_key_invalid, "proxy", "I", "interface I {}"),
+        (
+            struct_as_key_invalid,
+            "struct",
+            "MyStruct",
+            "struct MyStruct { }"
+        ),
+        (
+            struct_with_tags_as_key_invalid,
+            "struct",
+            "MyTaggedStruct",
+            "struct MyTaggedStruct { a: tag(1) int32 }"
+        ),
+        (
+            exception_as_key_invalid,
+            "exception",
+            "MyException",
+            "exception MyException { }"
+        ),
+        (trait_as_key_invalid, "trait", "MyTrait", "trait MyTrait"),
+        (
+            custom_as_key_invalid,
+            "custom",
+            "MyCustom",
+            "custom MyCustom"
+        )
+    );
+
+    /// Valid dictionary key types test.
+    macro_rules! test_simple_dictionary_key_types {
+        ($(($test_name:ident, $key_type:expr)),*) => {
+            $(
+                #[test]
+                fn $test_name() {
+                    // Arrange
+                    let slice = format!(
+                        "
+                        encoding = 2;
+                        module Test;
+                        typealias MyDict = dictionary<{key_type}, int32>;
+                        ",
+                        key_type=$key_type
+                    );
+
+                    // Act
+                    let error_reporter = parse_for_errors(&slice);
+
+                    // Assert
+                    assert!(!error_reporter.has_errors(true));
+                }
+            )*
+        }
+    }
+
+    // Valid dictionary key types test cases.
+    test_simple_dictionary_key_types!(
+        (uint8_as_key_valid, "uint8"),
+        (uint16_as_key_valid, "uint16"),
+        (uint32_as_key_valid, "uint32"),
+        (uint64_as_key_valid, "uint64"),
+        (int8_as_key_valid, "int8"),
+        (int16_as_key_valid, "int16"),
+        (int32_as_key_valid, "int32"),
+        (int64_as_key_valid, "int64"),
+        (varint32_as_key_valid, "varint32"),
+        (varuint32_as_key_valid, "varuint32"),
+        (varint62_as_key_valid, "varint62"),
+        (varuint62_as_key_valid, "varuint62"),
+        (string_as_key_valid, "string"),
+        (bool_as_key_valid, "bool")
+    );
+}
