5420bafe03537318aa9d6881865c418eda6e23c6
Joe George <joe@zeroc.com>
2022-05-02 15:55:19 -0400

    Move inheritance tests to new file

--- tests/interfaces/mod.rs
+++ tests/interfaces/mod.rs
@@ -1,5 +1,6 @@
 // Copyright (c) ZeroC, Inc. All rights reserved.
 
+mod inheritance;
 mod operations;
 
 use crate::helpers::parsing_helpers::*;
@@ -53,76 +54,4 @@ fn can_have_multiple_operation() {
     let interface_def = interface_ptr.borrow();
 
     assert_eq!(interface_def.operations().len(), 3);
-}
-
-#[test]
-fn supports_single_inheritance() {
-    let slice = "
-        module Test;
-        interface I {}
-        interface J : I {}
-    ";
-
-    let ast = parse_for_ast(slice);
-    let interface_i_ptr = ast.find_typed_type::<Interface>("Test::I").unwrap();
-    let interface_j_ptr = ast.find_typed_type::<Interface>("Test::J").unwrap();
-    let interface_i_def = interface_i_ptr.borrow();
-    let interface_j_def = interface_j_ptr.borrow();
-    let interface_j_bases = interface_j_def.base_interfaces();
-
-    assert!(interface_i_def.base_interfaces().is_empty());
-    assert_eq!(interface_j_bases.len(), 1);
-    assert_eq!(
-        interface_j_bases[0].module_scoped_identifier(),
-        interface_i_def.module_scoped_identifier(),
-    );
-}
-
-#[test]
-fn supports_multiple_inheritance() {
-    let slice = "
-        module Test;
-        interface I {}
-        interface J {}
-        interface K : I, J {}
-    ";
-
-    let ast = parse_for_ast(slice);
-    let interface_i_ptr = ast.find_typed_type::<Interface>("Test::I").unwrap();
-    let interface_j_ptr = ast.find_typed_type::<Interface>("Test::J").unwrap();
-    let interface_k_ptr = ast.find_typed_type::<Interface>("Test::K").unwrap();
-
-    let interface_i_def = interface_i_ptr.borrow();
-    let interface_j_def = interface_j_ptr.borrow();
-    let interface_k_def = interface_k_ptr.borrow();
-
-    let interface_k_bases = interface_k_def.base_interfaces();
-
-    assert!(interface_i_def.base_interfaces().is_empty());
-    assert!(interface_j_def.base_interfaces().is_empty());
-    assert_eq!(interface_k_bases.len(), 2);
-
-    assert_eq!(
-        interface_k_bases[0].module_scoped_identifier(),
-        interface_i_def.module_scoped_identifier(),
-    );
-
-    assert_eq!(
-        interface_k_bases[1].module_scoped_identifier(),
-        interface_j_def.module_scoped_identifier(),
-    );
-}
-
-#[test]
-fn must_inherit_from_interface() {
-    let slice = "
-    encoding = 1;
-    module Test;
-    class C {}
-    interface I : C {}
-    ";
-
-    let error_reporter = parse_for_errors(slice);
-
-    error_reporter.assert_errors(&["The Entity 'C' is not a valid type for this definition."])
 }
