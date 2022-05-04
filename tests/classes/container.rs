49ed7648423ae9714cb1bcd224d60c212073c0a1
Joe George <joe@zeroc.com>
2022-05-04 16:37:14 -0400

    Add class tests

---
+++ tests/classes/container.rs
@@ -0,0 +1,94 @@
+// Copyright (c) ZeroC, Inc. All rights reserved.
+
+use crate::assert_errors;
+use crate::helpers::parsing_helpers::{parse_for_ast, parse_for_errors};
+use slice::grammar::*;
+
+/// Verifies that classes can contain data members.
+#[test]
+fn can_contain_data_members() {
+    // Arrange
+    let slice = "
+    encoding = 1;
+    module Test;
+    class C
+    {
+        i: int32,
+        s: string,
+        b: bool,
+    }
+    ";
+
+    // Act
+    let ast = parse_for_ast(slice);
+
+    // Assert
+    let class_ptr = ast.find_typed_type::<Class>("Test::C").unwrap();
+    let class_def = class_ptr.borrow();
+    let data_members = class_def.members();
+
+    assert_eq!(data_members.len(), 3);
+    assert!(matches!(data_members[0].identifier(), "i"));
+    assert!(matches!(data_members[1].identifier(), "s"));
+    assert!(matches!(data_members[2].identifier(), "b"));
+
+    assert!(matches!(
+        data_members[0].data_type.concrete_type(),
+        Types::Primitive(Primitive::Int32)
+    ));
+    assert!(matches!(
+        data_members[1].data_type.concrete_type(),
+        Types::Primitive(Primitive::String)
+    ));
+    assert!(matches!(
+        data_members[2].data_type.concrete_type(),
+        Types::Primitive(Primitive::Bool)
+    ));
+}
+
+#[test]
+fn cycles_are_allowed() {
+    let slice = "
+    encoding = 1;
+    module Test;
+    class C
+    {
+        c: C,
+    }
+
+    class C1
+    {
+        c2: C2,
+    }
+
+    class C2
+    {
+        c1: C1,
+    }
+    ";
+
+    let error_reporter = parse_for_errors(slice);
+
+    assert_errors!(error_reporter);
+}
+
+/// Verifies that classes can be empty
+#[test]
+fn can_be_empty() {
+    // Arrange
+    let slice = "
+    encoding = 1;
+    module Test;
+    class C {}
+    ";
+
+    // Act
+    let ast = parse_for_ast(slice);
+
+    // Assert
+    let class_ptr = ast.find_typed_type::<Class>("Test::C").unwrap();
+    let class_def = class_ptr.borrow();
+    let data_members = class_def.members();
+
+    assert_eq!(data_members.len(), 0);
+}
--- tests/classes/encoding.rs
+++ tests/classes/encoding.rs
@@ -1,1 +1,24 @@
 // Copyright (c) ZeroC, Inc. All rights reserved.
+
+use crate::assert_errors;
+use crate::helpers::parsing_helpers::parse_for_errors;
+
+mod slice2 {
+    use super::*;
+
+    #[test]
+    fn unsupported_error() {
+        let slice = "
+            encoding = 2;
+            module Test;
+            class C {}
+        ";
+
+        let error_reporter = parse_for_errors(slice);
+
+        assert_errors!(error_reporter, &[
+            "classes are only supported by the Slice 1 encoding",
+            "file encoding was set to the Slice 2 encoding here:",
+        ]);
+    }
+}
