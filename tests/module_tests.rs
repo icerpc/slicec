8ba93088f978229d69eb2e5324311eda6676c860
Joe George <joe@zeroc.com>
2022-05-09 14:49:02 -0400

    Test that parsing fails is module is missing

--- tests/module_tests.rs
+++ tests/module_tests.rs
@@ -4,6 +4,7 @@ pub mod helpers;
 
 use crate::helpers::parsing_helpers::parse_for_ast;
 use slice::grammar::*;
+use slice::parse_from_string;
 
 mod module {
     use super::*;
@@ -55,5 +56,13 @@ mod module {
         let ast = parse_for_ast(slice);
 
         assert!(ast.find_typed_entity::<Module>("A::B::C::D").is_some());
+    }
+
+    #[test]
+    fn is_required() {
+        // TODO: better error message once we replace the parser
+        let slice = "custom C;";
+        let err = parse_from_string(slice).err();
+        assert!(err.is_some());
     }
 }
