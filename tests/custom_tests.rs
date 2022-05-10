9e9412e05f6ea85e75866ab91b60f25196900d72
Reece Humphreys <reece@zeroc.com>
2022-05-10 10:04:44 -0400

    Custom & Trait encoding tests

--- tests/custom_tests.rs
+++ tests/custom_tests.rs
@@ -2,22 +2,72 @@
 
 pub mod helpers;
 
-use helpers::parsing_helpers::parse_for_ast;
-use slice::grammar::*;
+mod custom {
 
-#[test]
-fn is_resolvable_as_an_entity() {
-    let slice = "
-    module Test;
-    custom ACustomType;
-    ";
+    mod slice1 {
 
-    let ast = parse_for_ast(slice);
+        use crate::assert_errors;
+        use crate::helpers::parsing_helpers::parse_for_errors;
 
-    let custom_ptr = ast
-        .find_typed_entity::<CustomType>("Test::ACustomType")
-        .unwrap();
-    let custom = custom_ptr.borrow();
+        #[test]
+        fn is_not_supported() {
+            // Arrange
+            let slice = "
+            encoding = 1;
+            module Test;
+            custom ACustomType;
+            ";
 
-    assert_eq!(custom.identifier(), "ACustomType");
+            // Act
+            let error_reporter = parse_for_errors(slice);
+
+            // Assert
+            assert_errors!(error_reporter, &[
+                "custom types are not supported by the Slice 1 encoding",
+                "file encoding was set to the Slice 1 encoding here:",
+            ]);
+        }
+    }
+
+    mod slice2 {
+
+        use crate::assert_errors;
+        use crate::helpers::parsing_helpers::{parse_for_ast, parse_for_errors};
+        use slice::grammar::*;
+
+        #[test]
+        fn is_valid() {
+            // Arrange
+            let slice = "
+            module Test;
+            custom ACustomType;
+            ";
+
+            // Act
+            let errors = parse_for_errors(slice);
+
+            // Assert
+            assert_errors!(errors);
+        }
+
+        #[test]
+        fn is_resolvable_as_an_entity() {
+            // Arrange
+            let slice = "
+            module Test;
+            custom ACustomType;
+            ";
+
+            // Act
+            let ast = parse_for_ast(slice);
+
+            // Assert
+            let custom_ptr = ast
+                .find_typed_entity::<CustomType>("Test::ACustomType")
+                .unwrap();
+            let custom = custom_ptr.borrow();
+
+            assert_eq!(custom.identifier(), "ACustomType");
+        }
+    }
 }
