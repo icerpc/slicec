460ad153c34bdc23a03a02d02163cdcaa2379c81
Reece Humphreys <reece@zeroc.com>
2022-05-10 10:55:58 -0400

    Fixing test scoping

--- tests/custom_tests.rs
+++ tests/custom_tests.rs
@@ -4,70 +4,54 @@ pub mod helpers;
 
 mod custom {
 
-    mod slice1 {
+    use crate::helpers::parsing_helpers::parse_for_ast;
+    use slice::grammar::*;
 
-        use crate::assert_errors;
-        use crate::helpers::parsing_helpers::parse_for_errors;
+    mod encoding {
 
-        #[test]
-        fn is_not_supported() {
-            // Arrange
-            let slice = "
-            encoding = 1;
-            module Test;
-            custom ACustomType;
-            ";
+        mod slice1 {
 
-            // Act
-            let error_reporter = parse_for_errors(slice);
+            use crate::assert_errors;
+            use crate::helpers::parsing_helpers::parse_for_errors;
 
-            // Assert
-            assert_errors!(error_reporter, &[
-                "custom types are not supported by the Slice 1 encoding",
-                "file encoding was set to the Slice 1 encoding here:",
-            ]);
+            #[test]
+            fn is_not_supported() {
+                // Arrange
+                let slice = "
+                encoding = 1;
+                module Test;
+                custom ACustomType;
+                ";
+
+                // Act
+                let error_reporter = parse_for_errors(slice);
+
+                // Assert
+                assert_errors!(error_reporter, &[
+                    "custom types are not supported by the Slice 1 encoding",
+                    "file encoding was set to the Slice 1 encoding here:",
+                ]);
+            }
         }
     }
 
-    mod slice2 {
+    #[test]
+    fn type_parses() {
+        // Arrange
+        let slice = "
+        module Test;
+        custom ACustomType;
+        ";
 
-        use crate::assert_errors;
-        use crate::helpers::parsing_helpers::{parse_for_ast, parse_for_errors};
-        use slice::grammar::*;
+        // Act
+        let ast = parse_for_ast(slice);
 
-        #[test]
-        fn is_valid() {
-            // Arrange
-            let slice = "
-            module Test;
-            custom ACustomType;
-            ";
+        // Assert
+        let custom_ptr = ast
+            .find_typed_entity::<CustomType>("Test::ACustomType")
+            .unwrap();
+        let custom = custom_ptr.borrow();
 
-            // Act
-            let errors = parse_for_errors(slice);
-
-            // Assert
-            assert_errors!(errors);
-        }
-
-        #[test]
-        fn is_resolvable_as_an_entity() {
-            // Arrange
-            let slice = "
-            module Test;
-            custom ACustomType;
-            ";
-
-            // Act
-            let ast = parse_for_ast(slice);
-
-            // Assert
-            let custom_ptr = ast
-                .find_typed_entity::<CustomType>("Test::ACustomType")
-                .unwrap();
-            let custom = custom_ptr.borrow();
-
-            assert_eq!(custom.identifier(), "ACustomType");
-        }
+        assert_eq!(custom.identifier(), "ACustomType");
     }
 }
--- tests/trait_tests.rs
+++ tests/trait_tests.rs
@@ -4,68 +4,52 @@ pub mod helpers;
 
 mod traits {
 
-    mod slice1 {
+    use crate::helpers::parsing_helpers::parse_for_ast;
+    use slice::grammar::*;
 
-        use crate::assert_errors;
-        use crate::helpers::parsing_helpers::parse_for_errors;
+    mod encoding {
 
-        #[test]
-        fn are_not_supported() {
-            // Arrange
-            let slice = "
-            encoding = 1;
-            module Test;
-            trait ATrait;
-            ";
+        mod slice1 {
 
-            // Act
-            let error_reporter = parse_for_errors(slice);
+            use crate::assert_errors;
+            use crate::helpers::parsing_helpers::parse_for_errors;
 
-            // Assert
-            assert_errors!(error_reporter, &[
-                "traits are not supported by the Slice 1 encoding",
-                "file encoding was set to the Slice 1 encoding here:",
-            ]);
+            #[test]
+            fn are_not_supported() {
+                // Arrange
+                let slice = "
+                encoding = 1;
+                module Test;
+                trait ATrait;
+                ";
+
+                // Act
+                let error_reporter = parse_for_errors(slice);
+
+                // Assert
+                assert_errors!(error_reporter, &[
+                    "traits are not supported by the Slice 1 encoding",
+                    "file encoding was set to the Slice 1 encoding here:",
+                ]);
+            }
         }
     }
 
-    mod slice2 {
+    #[test]
+    fn type_parses() {
+        // Arrange
+        let slice = "
+        module Test;
+        trait ATrait;
+        ";
 
-        use crate::assert_errors;
-        use crate::helpers::parsing_helpers::{parse_for_ast, parse_for_errors};
-        use slice::grammar::*;
+        // Act
+        let ast = parse_for_ast(slice);
 
-        #[test]
-        fn are_valid() {
-            // Arrange
-            let slice = "
-            module Test;
-            trait ATrait;
-            ";
+        // Assert
+        let trait_ptr = ast.find_typed_entity::<Trait>("Test::ATrait").unwrap();
+        let trait_def = trait_ptr.borrow();
 
-            // Act
-            let error_reporter = parse_for_errors(slice);
-
-            // Assert
-            assert_errors!(error_reporter);
-        }
-
-        #[test]
-        fn are_resolvable_as_an_entity() {
-            // Arrange
-            let slice = "
-            module Test;
-            trait ATrait;
-            ";
-
-            // Act
-            let ast = parse_for_ast(slice);
-
-            // Assert
-            let trait_ptr = ast.find_typed_entity::<Trait>("Test::ATrait").unwrap();
-            let trait_def = trait_ptr.borrow();
-
-            assert_eq!(trait_def.identifier(), "ATrait");
-        }
+        assert_eq!(trait_def.identifier(), "ATrait");
     }
 }
