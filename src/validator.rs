b4e21d207b9fde35a8bffbb42e38d9a97f7ddcd7
Reece Humphreys <reece@zeroc.com>
2022-05-24 16:41:22 -0400

    Cleanup

--- src/validator.rs
+++ src/validator.rs
@@ -4,7 +4,7 @@ use crate::ast::Ast;
 use crate::error::ErrorReporter;
 use crate::grammar::*;
 use crate::slice_file::SliceFile;
-use crate::validators::{AttributeValidator, EnumValidator, TagValidator};
+use crate::validators::{AttributeValidator, DictionaryValidator, EnumValidator, TagValidator};
 use crate::visitor::Visitor;
 use std::collections::HashMap;
 
@@ -15,6 +15,7 @@ pub(crate) struct Validator<'a> {
 }
 
 impl Validator<'_> {
+    /// This method is responsible for visiting each slice file with the various validators.
     pub fn validate(&mut self, slice_files: &HashMap<String, SliceFile>) {
         for slice_file in slice_files.values() {
             slice_file.visit_with(self);
@@ -24,123 +25,11 @@ impl Validator<'_> {
                 encoding: slice_file.encoding(),
             });
             slice_file.visit_with(&mut TagValidator { error_reporter: self.error_reporter });
+            let dictionary_validator =
+                &mut DictionaryValidator { error_reporter: self.error_reporter, ast: self.ast };
+            slice_file.visit_with(dictionary_validator);
+            dictionary_validator.validate_dictionary_key_types();
         }
-        self.validate_dictionary_key_types();
-    }
-
-    fn validate_dictionary_key_types(&mut self) {
-        for type_ptr in &self.ast.anonymous_types {
-            if let Types::Dictionary(dictionary) = type_ptr.borrow().concrete_type() {
-                self.check_dictionary_key_type(&dictionary.key_type);
-            }
-        }
-    }
-
-    fn check_dictionary_key_type(&mut self, type_ref: &TypeRef) -> bool {
-        // Optional types cannot be used as dictionary keys.
-        if type_ref.is_optional {
-            self.error_reporter.report_error(
-                "invalid dictionary key type: optional types cannot be used as a dictionary key type".to_owned(),
-                Some(&type_ref.location),
-            );
-            return false;
-        }
-
-        let definition = type_ref.definition();
-        let (is_valid, named_symbol): (bool, Option<&dyn NamedSymbol>) = match definition
-            .concrete_type()
-        {
-            Types::Struct(struct_def) => {
-                // Only compact structs can be used for dictionary keys.
-                if !struct_def.is_compact {
-                    self.error_reporter.report_error(
-                        "invalid dictionary key type: structs must be compact to be used as a dictionary key type".to_owned(),
-                        Some(&type_ref.location),
-                    );
-                    self.error_reporter.report_note(
-                        format!("struct '{}' is defined here:", struct_def.identifier()),
-                        Some(&struct_def.location),
-                    );
-                    return false;
-                }
-
-                // Check that all the data members of the struct are also valid key types.
-                let mut contains_invalid_key_types = false;
-                for member in struct_def.members() {
-                    if !self.check_dictionary_key_type(member.data_type()) {
-                        self.error_reporter.report_error(
-                            format!(
-                                "data member '{}' cannot be used as a dictionary key type",
-                                member.identifier(),
-                            ),
-                            Some(&member.location),
-                        );
-                        contains_invalid_key_types = true;
-                    }
-                }
-
-                if contains_invalid_key_types {
-                    self.error_reporter.report_error(
-                        format!(
-                            "invalid dictionary key type: struct '{}' contains members that cannot be used as a dictionary key type",
-                            struct_def.identifier(),
-                        ),
-                        Some(&type_ref.location),
-                    );
-                    self.error_reporter.report_note(
-                        format!("struct '{}' is defined here:", struct_def.identifier()),
-                        Some(&struct_def.location),
-                    );
-                    return false;
-                }
-                return true;
-            }
-            Types::Class(class_def) => (false, Some(class_def)),
-            Types::Exception(exception_def) => (false, Some(exception_def)),
-            Types::Interface(interface_def) => (false, Some(interface_def)),
-            Types::Enum(_) => (true, None),
-            Types::Trait(trait_def) => (false, Some(trait_def)),
-            Types::CustomType(_) => (true, None),
-            Types::Sequence(_) => (false, None),
-            Types::Dictionary(_) => (false, None),
-            Types::Primitive(primitive) => (
-                !matches!(
-                    primitive,
-                    Primitive::Float32 | Primitive::Float64 | Primitive::AnyClass
-                ),
-                None,
-            ),
-        };
-
-        if !is_valid {
-            let pluralized_kind = match definition.concrete_type() {
-                Types::Primitive(_) => definition.kind().to_owned(),
-                Types::Class(_) => "classes".to_owned(),
-                Types::Dictionary(_) => "dictionaries".to_owned(),
-                _ => definition.kind().to_owned() + "s",
-            };
-
-            self.error_reporter.report_error(
-                format!(
-                    "invalid dictionary key type: {} cannot be used as a dictionary key type",
-                    pluralized_kind,
-                ),
-                Some(&type_ref.location),
-            );
-
-            // If the key type is a user-defined type, point to where it was defined.
-            if let Some(named_symbol_def) = named_symbol {
-                self.error_reporter.report_note(
-                    format!(
-                        "{} '{}' is defined here:",
-                        named_symbol_def.kind(),
-                        named_symbol_def.identifier(),
-                    ),
-                    Some(named_symbol_def.location()),
-                );
-            }
-        }
-        is_valid
     }
 
     fn validate_stream_member(&mut self, members: Vec<&Parameter>) {
