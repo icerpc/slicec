c572b232cae71d7ae0c03a724cd90db9db40a58d
Reece Humphreys <reece@zeroc.com>
2022-05-25 09:15:43 -0400

    Validator restructure (#157)

---
+++ src/validators/dictionary.rs
@@ -0,0 +1,131 @@
+// Copyright (c) ZeroC, Inc. All rights reserved.
+
+use crate::ast::Ast;
+use crate::error::ErrorReporter;
+use crate::grammar::*;
+use crate::visitor::Visitor;
+
+#[derive(Debug)]
+pub struct DictionaryValidator<'a> {
+    pub error_reporter: &'a mut ErrorReporter,
+    pub ast: &'a Ast,
+}
+
+impl DictionaryValidator<'_> {
+    pub fn validate_dictionary_key_types(&mut self) {
+        for type_ptr in &self.ast.anonymous_types {
+            if let Types::Dictionary(dictionary) = type_ptr.borrow().concrete_type() {
+                self.check_dictionary_key_type(&dictionary.key_type);
+            }
+        }
+    }
+
+    fn check_dictionary_key_type(&mut self, type_ref: &TypeRef) -> bool {
+        // Optional types cannot be used as dictionary keys.
+        if type_ref.is_optional {
+            self.error_reporter.report_error(
+                "invalid dictionary key type: optional types cannot be used as a dictionary key type".to_owned(),
+                Some(&type_ref.location),
+            );
+            return false;
+        }
+
+        let definition = type_ref.definition();
+        let (is_valid, named_symbol): (bool, Option<&dyn NamedSymbol>) = match definition
+            .concrete_type()
+        {
+            Types::Struct(struct_def) => {
+                // Only compact structs can be used for dictionary keys.
+                if !struct_def.is_compact {
+                    self.error_reporter.report_error(
+                        "invalid dictionary key type: structs must be compact to be used as a dictionary key type".to_owned(),
+                        Some(&type_ref.location),
+                    );
+                    self.error_reporter.report_note(
+                        format!("struct '{}' is defined here:", struct_def.identifier()),
+                        Some(&struct_def.location),
+                    );
+                    return false;
+                }
+
+                // Check that all the data members of the struct are also valid key types.
+                let mut contains_invalid_key_types = false;
+                for member in struct_def.members() {
+                    if !self.check_dictionary_key_type(member.data_type()) {
+                        self.error_reporter.report_error(
+                            format!(
+                                "data member '{}' cannot be used as a dictionary key type",
+                                member.identifier(),
+                            ),
+                            Some(&member.location),
+                        );
+                        contains_invalid_key_types = true;
+                    }
+                }
+
+                if contains_invalid_key_types {
+                    self.error_reporter.report_error(
+                        format!(
+                            "invalid dictionary key type: struct '{}' contains members that cannot be used as a dictionary key type",
+                            struct_def.identifier(),
+                        ),
+                        Some(&type_ref.location),
+                    );
+                    self.error_reporter.report_note(
+                        format!("struct '{}' is defined here:", struct_def.identifier()),
+                        Some(&struct_def.location),
+                    );
+                    return false;
+                }
+                return true;
+            }
+            Types::Class(class_def) => (false, Some(class_def)),
+            Types::Exception(exception_def) => (false, Some(exception_def)),
+            Types::Interface(interface_def) => (false, Some(interface_def)),
+            Types::Enum(_) => (true, None),
+            Types::Trait(trait_def) => (false, Some(trait_def)),
+            Types::CustomType(_) => (true, None),
+            Types::Sequence(_) => (false, None),
+            Types::Dictionary(_) => (false, None),
+            Types::Primitive(primitive) => (
+                !matches!(
+                    primitive,
+                    Primitive::Float32 | Primitive::Float64 | Primitive::AnyClass
+                ),
+                None,
+            ),
+        };
+
+        if !is_valid {
+            let pluralized_kind = match definition.concrete_type() {
+                Types::Primitive(_) => definition.kind().to_owned(),
+                Types::Class(_) => "classes".to_owned(),
+                Types::Dictionary(_) => "dictionaries".to_owned(),
+                _ => definition.kind().to_owned() + "s",
+            };
+
+            self.error_reporter.report_error(
+                format!(
+                    "invalid dictionary key type: {} cannot be used as a dictionary key type",
+                    pluralized_kind,
+                ),
+                Some(&type_ref.location),
+            );
+
+            // If the key type is a user-defined type, point to where it was defined.
+            if let Some(named_symbol_def) = named_symbol {
+                self.error_reporter.report_note(
+                    format!(
+                        "{} '{}' is defined here:",
+                        named_symbol_def.kind(),
+                        named_symbol_def.identifier(),
+                    ),
+                    Some(named_symbol_def.location()),
+                );
+            }
+        }
+        is_valid
+    }
+}
+
+impl<'a> Visitor for DictionaryValidator<'a> {}
---
+++ src/validators/tag.rs
@@ -0,0 +1,209 @@
+// Copyright (c) ZeroC, Inc. All rights reserved.
+
+use crate::code_gen_util::get_sorted_members;
+use crate::error::ErrorReporter;
+use crate::grammar::*;
+use crate::visitor::Visitor;
+
+#[derive(Debug)]
+pub struct TagValidator<'a> {
+    pub error_reporter: &'a mut ErrorReporter,
+}
+
+impl TagValidator<'_> {
+    // Validate that tagged parameters must follow the required parameters.
+    fn parameter_order(&mut self, parameters: &[&Parameter]) {
+        // Folding is used to have an accumulator called `seen` that is set to true once a tagged
+        // parameter is found. If `seen` is true on a successive iteration and the parameter has
+        // no tag then we have a required parameter after a tagged parameter.
+        parameters.iter().fold(false, |seen, parameter| {
+            match parameter.tag {
+                Some(_) => true,
+                None if seen => {
+                    self.error_reporter.report_error(
+                        format!(
+                            "invalid parameter `{}`: required parameters must precede tagged parameters",
+                            parameter.identifier()
+                        ),
+                        Some(&parameter.data_type.location)
+                    );
+                    true
+                },
+                None => false
+            }
+        });
+    }
+
+    /// Validate that tags cannot be used in compact structs.
+    fn compact_structs_cannot_contain_tags(&mut self, struct_def: &Struct) {
+        // Compact structs must be non-empty.
+        if !struct_def.members.is_empty() {
+            // Compact structs cannot have tagged data members.
+            let mut has_tags = false;
+            for member in struct_def.members() {
+                if member.tag.is_some() {
+                    self.error_reporter.report_error(
+                        "tagged data members are not supported in compact structs\n\
+                            consider removing the tag, or making the struct non-compact"
+                            .to_owned(),
+                        Some(member.location()),
+                    );
+                    has_tags = true;
+                }
+            }
+
+            if has_tags {
+                self.error_reporter.report_note(
+                    format!(
+                        "struct '{}' is declared compact here",
+                        struct_def.identifier()
+                    ),
+                    Some(&struct_def.location),
+                );
+            }
+        }
+    }
+
+    /// Validates that the tags are unique.
+    fn tags_are_unique(&mut self, members: &[&impl Member]) {
+        // The tagged members must be sorted by value first as we are using windowing to check the
+        // n + 1 tagged member against the n tagged member. If the tags are sorted by value then
+        // the windowing will reveal any duplicate tags.
+        let (_, tagged_members) = get_sorted_members(members);
+        tagged_members.windows(2).for_each(|window| {
+            if window[0].tag() == window[1].tag() {
+                self.error_reporter.report_error(
+                    format!(
+                        "invalid tag on member `{}`: tags must be unique",
+                        &window[1].identifier()
+                    ),
+                    Some(window[1].location()),
+                );
+                self.error_reporter.report_error(
+                    format!(
+                        "The data member `{}` has previous used the tag value `{}`",
+                        &window[0].identifier(),
+                        window[0].tag().unwrap()
+                    ),
+                    Some(window[0].location()),
+                );
+            }
+        });
+    }
+
+    /// Validate that the data type of the tagged member is optional.
+    fn have_optional_types(&mut self, members: &[&impl Member]) {
+        let tagged_members = members
+            .iter()
+            .filter(|member| member.tag().is_some())
+            .clone()
+            .collect::<Vec<_>>();
+
+        // Validate that tagged members are optional.
+        for member in tagged_members {
+            if !member.data_type().is_optional {
+                self.error_reporter.report_error(
+                    format!(
+                        "invalid member `{}`: tagged members must be optional",
+                        member.identifier()
+                    )
+                    .to_owned(),
+                    Some(member.location()),
+                );
+            }
+        }
+    }
+
+    /// Validate that classes cannot be tagged.
+    fn cannot_tag_classes(&mut self, members: &[&impl Member]) {
+        let tagged_members = members
+            .iter()
+            .filter(|member| member.tag().is_some())
+            .clone()
+            .collect::<Vec<_>>();
+
+        for member in tagged_members {
+            if member.data_type().definition().is_class_type() {
+                self.error_reporter.report_error(
+                    format!(
+                        "invalid member `{}`: tagged members cannot be classes",
+                        member.identifier()
+                    )
+                    .to_owned(),
+                    Some(member.location()),
+                );
+            }
+        }
+    }
+
+    /// Validate that tagged container types cannot contain class members.
+    fn tagged_containers_cannot_contain_classes(&mut self, members: &[&impl Member]) {
+        let tagged_members = members
+            .iter()
+            .filter(|member| member.tag().is_some())
+            .clone()
+            .collect::<Vec<_>>();
+
+        for member in tagged_members {
+            // TODO: This works but the uses_classes method is not intuitive. Should be renamed
+            // or changed so that if a class contains no members it returns false.
+            if match member.data_type().concrete_type() {
+                Types::Class(c) => {
+                    if c.members().is_empty() {
+                        false
+                    } else {
+                        !c.members()
+                            .iter()
+                            .any(|m| m.data_type().definition().uses_classes())
+                    }
+                }
+                _ => member.data_type().definition().uses_classes(),
+            } {
+                self.error_reporter.report_error(
+                    format!(
+                        "invalid type `{}`: tagged members cannot contain classes",
+                        member.identifier()
+                    )
+                    .to_owned(),
+                    Some(member.location()),
+                );
+            }
+        }
+    }
+}
+
+impl<'a> Visitor for TagValidator<'a> {
+    fn visit_exception_start(&mut self, exception_def: &Exception) {
+        self.tags_are_unique(&exception_def.members());
+        self.have_optional_types(&exception_def.members());
+        self.tagged_containers_cannot_contain_classes(&exception_def.members());
+        self.cannot_tag_classes(&exception_def.members());
+    }
+
+    fn visit_struct_start(&mut self, struct_def: &Struct) {
+        if struct_def.is_compact {
+            self.compact_structs_cannot_contain_tags(struct_def)
+        } else {
+            // Tags can only exist on non compact structs.
+            self.tags_are_unique(&struct_def.members());
+            self.have_optional_types(&struct_def.members());
+        }
+    }
+
+    fn visit_class_start(&mut self, class_def: &Class) {
+        self.tags_are_unique(&class_def.members());
+        self.have_optional_types(&class_def.members());
+        self.tagged_containers_cannot_contain_classes(&class_def.members());
+        self.cannot_tag_classes(&class_def.members());
+    }
+
+    fn visit_operation_start(&mut self, operation_def: &Operation) {
+        for member_list in [operation_def.parameters(), operation_def.return_members()].iter() {
+            self.parameter_order(member_list);
+            self.have_optional_types(member_list);
+            self.tags_are_unique(member_list);
+            self.tagged_containers_cannot_contain_classes(member_list);
+            self.cannot_tag_classes(member_list);
+        }
+    }
+}
