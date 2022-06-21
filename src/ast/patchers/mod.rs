22bc3e3de8e714fac5b870c22e9eb2581ce5cb41
Austin Henriksen <austin@zeroc.com>
2022-06-21 10:46:29 -0400

    Fixed the merge to have my actual changes.

--- src/ast/patchers/mod.rs
+++ src/ast/patchers/mod.rs
@@ -1,13 +1,7 @@
 // Copyright (c) ZeroC, Inc. All rights reserved.
 
-<<<<<<< HEAD
 //! TODO write a doc comment for the module.
 
 pub mod encoding_patcher;
 pub mod parent_patcher; // TODO remove this when we switch to LALRpop.
 pub mod type_ref_patcher;
-=======
-pub mod encoding_patcher;
-pub mod parent_patcher;
-pub mod type_patcher;
->>>>>>> eeface3f18dd2e3c7f6234476994d89d8b13ca33
