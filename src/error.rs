f5d4ea31a43d8f7d9cc65c4bbb1ac66bfeeb3a6e
Austin Henriksen <austin@zeroc.com>
2022-07-05 11:49:50 -0400

    Renamed 'print_errors' to 'emit_errors'.

--- src/error.rs
+++ src/error.rs
@@ -90,7 +90,7 @@ impl ErrorReporter {
     }
 
     /// Writes the errors stored to stderr, along with any locations and snippets.
-    pub fn print_errors(self, slice_files: &HashMap<String, SliceFile>) {
+    pub fn emit_errors(self, slice_files: &HashMap<String, SliceFile>) {
         for error in self.errors.into_iter() {
             let prefix = match error.severity {
                 ErrorLevel::Note => "note",
