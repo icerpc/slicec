fe444716c6e15df0ae95080a9e6fdd130191e785
Reece Humphreys <reecewh@icloud.com>
2022-08-08 11:46:22 -0400

    Update slice.rs

--- src/parser/slice.rs
+++ src/parser/slice.rs
@@ -921,7 +921,6 @@ impl<'a> SliceParser<'a> {
             },
             [line_doc_comment(comments)..] => {
                 // Merge all the line comments together.
-
                 let combined = comments.collect::<Vec<String>>().join("\n");
                 Some(CommentParser::parse_doc_comment(&combined, span))
             },
