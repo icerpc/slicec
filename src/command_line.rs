2d58b8bda71c177ffe75db2bb96526506ceb88ac
Reece Humphreys <reece@zeroc.com>
2022-09-28 10:14:01 -0400

    Migration from structopt to clap

--- src/command_line.rs
+++ src/command_line.rs
@@ -1,56 +1,53 @@
 // Copyright (c) ZeroC, Inc. All rights reserved.
 
+use clap::{Parser, ValueEnum};
 use serde::Serialize;
-use structopt::clap::arg_enum;
-use structopt::StructOpt;
 
-// Note: StructOpt uses the doc-comments of fields to populate the '--help' output of slice-xxx.
+// Note: clap uses the doc-comments of fields to populate the '--help' output of slice-xxx.
 //       boolean flags automatically default to false, and strings automatically default to empty.
 
 /// This struct is responsible for parsing the command line options common to all slice compilers.
-/// The option parsing capabilities are generated on the struct by the `StructOpt` macro.
-#[derive(Debug, StructOpt)]
-#[structopt(rename_all = "kebab-case")] // Each compiler sets its own `about` message.
+/// The option parsing capabilities are generated on the struct by the `clap` macro.
+#[derive(Parser, Debug)]
+#[clap(rename_all = "kebab-case")] // Each compiler sets its own `about` message.
 pub struct SliceOptions {
     /// List of slice files to compile.
-    #[structopt(required = true)]
+    #[clap(required = true, value_parser)] // TODO: Add validation that the file is a .slice file
     pub sources: Vec<String>,
 
     /// Files that are needed for referencing, but that no code should be generated for.
-    #[structopt(short = "R", long, number_of_values = 1, multiple = true)]
+    #[clap(short = 'R', long, number_of_values = 1, multiple = true, value_parser)]
+    // TODO: Add validation that the file is a .slice file
     pub references: Vec<String>,
 
     /// Instructs the compiler to treat warnings as errors.
-    #[structopt(short, long)]
+    #[clap(short, long, value_parser)]
     pub warn_as_error: bool,
 
     /// Validates input files without generating code for them.
-    #[structopt(long)]
+    #[clap(long, value_parser)]
     pub validate: bool,
 
     /// Output directory for generated code, defaults to the current working directory.
-    #[structopt(long)]
+    #[clap(long, value_parser)]
     pub output_dir: Option<String>,
 
     /// Output format for emitted errors,
-    #[structopt(case_insensitive = true, default_value = "human", long, possible_values = &["human", "json"])]
+    #[clap(arg_enum, case_insensitive = true, default_value_t = DiagnosticFormat::Human, long, value_parser)]
     pub diagnostic_format: DiagnosticFormat,
 
     /// Disables ANSI escape code for diagnostic output.
-    #[structopt(long)]
+    #[clap(long, value_parser)]
     pub disable_color: bool,
 }
 
-arg_enum! {
-
-    /// This enum is used to specify the format for emitted diagnostics.
-    ///
-    /// # Variants
-    /// * Human - Any emitted diagnostics will be printed to the console with an easily readable format.
-    /// * Json - Any emitted diagnostics will be serialized as JSON objects and printed to the console.
-    #[derive(Clone, Copy, Debug, Eq, PartialEq, Serialize)]
-    pub enum DiagnosticFormat {
-        Human,
-        Json,
-    }
+/// This enum is used to specify the format for emitted diagnostics.
+///
+/// # Variants
+/// * Human - Any emitted diagnostics will be printed to the console with an easily readable format.
+/// * Json - Any emitted diagnostics will be serialized as JSON objects and printed to the console.
+#[derive(Clone, Copy, Debug, Eq, PartialEq, Serialize, ValueEnum)]
+pub enum DiagnosticFormat {
+    Human,
+    Json,
 }
