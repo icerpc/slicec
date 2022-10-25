fcb42d52e1ee9feddb858d9bf0b14f3958ecf50d
Austin Henriksen <austin@zeroc.com>
2022-10-25 16:14:45 -0400

    Simplified parsing entry-point.

--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -1,20 +1,16 @@
 // Copyright (c) ZeroC, Inc. All rights reserved.
 
-// TODO most of this module was just copy & pasted from the original implementation so that people
-// can start using the newer implementation sooner.
-
 pub mod comments;
 mod cycle_detection;
-mod slice;
 
 use crate::ast::Ast;
 use crate::command_line::{DiagnosticFormat, SliceOptions};
-use crate::diagnostics::DiagnosticReporter;
+use crate::diagnostics::{DiagnosticReporter, Error, ErrorKind};
 use crate::grammar::attributes;
 use crate::parse_result::{ParsedData, ParserResult};
 use crate::slice_file::SliceFile;
 
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 use std::path::PathBuf;
 use std::{fs, io};
 
@@ -23,14 +19,11 @@ use std::{fs, io};
 // patching). Accessing ANY data, or calling ANY methods before this point may result in panics or
 // undefined behavior.
 
-// TODO This module is a mess.
-
 pub fn parse_files(options: &SliceOptions) -> ParserResult {
-    let mut ast = Ast::create();
-    let mut diagnostic_reporter = DiagnosticReporter::new(options);
-
-    let mut parser = slice::SliceParser {
-        diagnostic_reporter: &mut diagnostic_reporter,
+    let mut data = ParsedData {
+        ast: Ast::create(),
+        diagnostic_reporter: DiagnosticReporter::new(options),
+        files: HashMap::new(),
     };
 
     let source_files = find_slice_files(&options.sources);
@@ -41,33 +34,39 @@ pub fn parse_files(options: &SliceOptions) -> ParserResult {
     reference_files.sort();
     reference_files.dedup();
 
-    let mut slice_files = HashMap::new();
-
     for path in source_files {
-        if let Some(slice_file) = parser.try_parse_file(&path, true, &mut ast) {
-            slice_files.insert(path.to_owned(), slice_file);
+        if let Some(slice_file) = try_parse_file(&path, true, &mut data) {
+            data.files.insert(path.to_owned(), slice_file);
         }
     }
 
     for path in reference_files {
-        if let Some(slice_file) = parser.try_parse_file(&path, false, &mut ast) {
-            slice_files.insert(path.to_owned(), slice_file);
+        if let Some(slice_file) = try_parse_file(&path, false, &mut data) {
+            data.files.insert(path.to_owned(), slice_file);
         }
     }
 
     // Update the diagnostic reporter with the slice files that contain the file level ignoreWarnings attribute.
-    diagnostic_reporter.file_level_ignored_warnings = file_ignored_warnings_map(&slice_files);
-    let parsed_data = ParsedData {
-        ast,
-        files: slice_files,
-        diagnostic_reporter,
-    };
+    data.diagnostic_reporter.file_level_ignored_warnings = file_ignored_warnings_map(&data.files);
 
-    patch_ast(parsed_data)
+    patch_ast(data)
+}
+
+fn try_parse_file(file: &str, is_source: bool, data: &mut ParsedData) -> Option<SliceFile> {
+    match fs::read_to_string(file) {
+        Ok(raw_text) => {
+            // The parser emits errors through `DiagnosticReporter` on it's own, so we don't need to handle them.
+            try_parse_string(file, &raw_text, is_source, data).ok()
+        }
+        Err(err) => {
+            data.diagnostic_reporter
+                .report_error(Error::new(ErrorKind::Syntax(err.to_string()), None));
+            None
+        }
+    }
 }
 
 pub fn parse_strings(inputs: &[&str], options: Option<SliceOptions>) -> ParserResult {
-    let mut ast = Ast::create();
     let slice_options = options.unwrap_or(SliceOptions {
         sources: vec![],
         references: vec![],
@@ -77,28 +76,53 @@ pub fn parse_strings(inputs: &[&str], options: Option<SliceOptions>) -> ParserRe
         validate: false,
         output_dir: None,
     });
-    let mut diagnostic_reporter = DiagnosticReporter::new(&slice_options);
-    let mut parser = slice::SliceParser {
-        diagnostic_reporter: &mut diagnostic_reporter,
+
+    let mut data = ParsedData {
+        ast: Ast::create(),
+        diagnostic_reporter: DiagnosticReporter::new(&slice_options),
+        files: HashMap::new(),
     };
 
-    let mut slice_files = HashMap::new();
-
     for (i, input) in inputs.iter().enumerate() {
-        if let Some(slice_file) = parser.try_parse_string(&format!("string-{}", i), input, &mut ast) {
-            slice_files.insert(slice_file.filename.clone(), slice_file);
+        let name = format!("string-{}", i);
+        if let Ok(slice_file) = try_parse_string(&name, input, false, &mut data) {
+            data.files.insert(slice_file.filename.clone(), slice_file);
         }
     }
 
     // Update the diagnostic reporter with the slice files that contain the file level ignoreWarnings attribute.
-    diagnostic_reporter.file_level_ignored_warnings = file_ignored_warnings_map(&slice_files);
-    let parsed_data = ParsedData {
-        ast,
-        files: slice_files,
-        diagnostic_reporter,
-    };
+    data.diagnostic_reporter.file_level_ignored_warnings = file_ignored_warnings_map(&data.files);
 
-    patch_ast(parsed_data)
+    patch_ast(data)
+}
+
+fn try_parse_string(file: &str, raw_text: &str, is_source: bool, data: &mut ParsedData) -> Result<SliceFile, ()> {
+    let ast = &mut data.ast;
+    let diagnostic_reporter = &mut data.diagnostic_reporter;
+
+    // Run the raw text through the preprocessor.
+    let mut defined_symbols = HashSet::new();
+    let mut preprocessor = crate::parsers::Preprocessor::new(file, &mut defined_symbols, diagnostic_reporter);
+    let preprocessed_text = preprocessor.parse_slice_file(raw_text)?;
+
+    // Run the preprocessed text through the parser.
+    let mut parser = crate::parsers::Parser::new(file, ast, diagnostic_reporter);
+    let (file_encoding, file_attributes, modules) = parser.parse_slice_file(preprocessed_text)?;
+
+    // Add the top-level-modules into the AST, but keep `WeakPtr`s to them.
+    let top_level_modules = modules
+        .into_iter()
+        .map(|module| ast.add_named_element(module))
+        .collect::<Vec<_>>();
+
+    Ok(SliceFile::new(
+        file.to_owned(),
+        raw_text.to_owned(),
+        top_level_modules,
+        file_attributes,
+        file_encoding,
+        is_source,
+    ))
 }
 
 fn patch_ast(mut parsed_data: ParsedData) -> ParserResult {
