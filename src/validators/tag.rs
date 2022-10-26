c3eb7138baf74297651b343a5cbfb0931652dc69
Austin Henriksen <austin@zeroc.com>
2022-10-26 11:15:45 -0400

    simplified class tagging validation.

--- src/validators/tag.rs
+++ src/validators/tag.rs
@@ -7,8 +7,7 @@ use crate::validators::{ValidationChain, Validator};
 pub fn tag_validators() -> ValidationChain {
     vec![
         Validator::Members(tags_have_optional_types),
-        Validator::Members(tagged_containers_cannot_contain_classes),
-        Validator::Members(cannot_tag_classes),
+        Validator::Members(tagged_members_cannot_use_classes),
         Validator::Members(tags_are_unique),
         Validator::Struct(compact_structs_cannot_contain_tags),
         Validator::Parameters(parameter_order),
@@ -101,49 +100,16 @@ fn tags_have_optional_types(members: Vec<&dyn Member>, diagnostic_reporter: &mut
     }
 }
 
-/// Validate that classes cannot be tagged.
-fn cannot_tag_classes(members: Vec<&dyn Member>, diagnostic_reporter: &mut DiagnosticReporter) {
-    let tagged_members = members
-        .iter()
-        .filter(|member| member.tag().is_some())
-        .clone()
-        .collect::<Vec<_>>();
-
-    for member in tagged_members {
-        if member.data_type().definition().is_class_type() {
-            diagnostic_reporter.report_error(Error::new(
-                ErrorKind::CannotTagClass(member.identifier().to_owned()),
-                Some(member.span()),
-            ));
-        }
-    }
-}
-
-/// Validate that tagged container types cannot contain class members.
-fn tagged_containers_cannot_contain_classes(members: Vec<&dyn Member>, diagnostic_reporter: &mut DiagnosticReporter) {
-    let tagged_members = members
-        .iter()
-        .filter(|member| member.tag().is_some())
-        .clone()
-        .collect::<Vec<_>>();
-
-    for member in tagged_members {
-        // TODO: This works but the uses_classes method is not intuitive. Should be renamed
-        // or changed so that if a class contains no members it returns false.
-        if match member.data_type().concrete_type() {
-            Types::Class(c) => {
-                if c.members().is_empty() {
-                    false
-                } else {
-                    !c.members().iter().any(|m| m.data_type().definition().uses_classes())
-                }
-            }
-            _ => member.data_type().definition().uses_classes(),
-        } {
-            diagnostic_reporter.report_error(Error::new(
-                ErrorKind::CannotTagContainingClass(member.identifier().to_owned()),
-                Some(member.span()),
-            ));
+fn tagged_members_cannot_use_classes(members: Vec<&dyn Member>, diagnostic_reporter: &mut DiagnosticReporter) {
+    for member in members {
+        if member.is_tagged() && member.data_type().uses_classes() {
+            let identifier = member.identifier().to_owned();
+            let error_kind = if member.data_type().is_class_type() {
+                ErrorKind::CannotTagClass(identifier)
+            } else {
+                ErrorKind::CannotTagContainingClass(identifier)
+            };
+            diagnostic_reporter.report_error(Error::new(error_kind, Some(member.span())));
         }
     }
 }
