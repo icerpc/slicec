b3d8111e01f438f57bfb9dc4825ed4d749a19f85
Reece Humphreys <reece@zeroc.com>
2022-12-12 11:07:38 -0500

    Added escaped identifier tests

---
+++ tests/identifier_tests.rs
@@ -0,0 +1,48 @@
+// Copyright (c) ZeroC, Inc. All rights reserved.
+
+pub mod helpers;
+
+use helpers::parsing_helpers::parse_for_ast;
+use slice::grammar::{Enum, Exception, Interface, Struct};
+
+#[test]
+fn escaped_keywords() {
+    // Arrange
+    let slice = r#"
+        module \module;
+        interface \interface {}
+        exception \exception {}
+        struct \struct {}
+        enum \enum {}
+    "#;
+
+    // Act
+    let ast = parse_for_ast(slice);
+
+    // Assert
+    assert!(ast.find_element::<Interface>("module::interface").is_ok());
+    assert!(ast.find_element::<Exception>("module::exception").is_ok());
+    assert!(ast.find_element::<Struct>("module::struct").is_ok());
+    assert!(ast.find_element::<Enum>("module::enum").is_ok());
+}
+
+#[test]
+fn escaped_identifiers() {
+    // Arrange
+    let slice = r#"
+        module \MyModule;
+        interface \MyInterface {}
+        exception \MyException {}
+        struct \MyStruct {}
+        enum \MyEnum {}
+    "#;
+
+    // Act
+    let ast = parse_for_ast(slice);
+
+    // Assert
+    assert!(ast.find_element::<Interface>("MyModule::MyInterface").is_ok());
+    assert!(ast.find_element::<Exception>("MyModule::MyException").is_ok());
+    assert!(ast.find_element::<Struct>("MyModule::MyStruct").is_ok());
+    assert!(ast.find_element::<Enum>("MyModule::MyEnum").is_ok());
+}
