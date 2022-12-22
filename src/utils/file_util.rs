d2c54bdd93808f294e2ac4bfe8343dfe20c5a235
Reece Humphreys <reece@zeroc.com>
2022-12-22 09:03:14 -0500

    Duplicate file filtering fixes (#390)

--- src/utils/file_util.rs
+++ src/utils/file_util.rs
@@ -4,26 +4,87 @@ use crate::command_line::SliceOptions;
 use crate::diagnostics::{DiagnosticReporter, Error, ErrorKind};
 use crate::slice_file::SliceFile;
 use std::collections::HashMap;
+use std::hash::{Hash, Hasher};
 use std::path::PathBuf;
 use std::{fs, io};
+
+/// A wrapper around a file path that implements Hash and Eq. This allows us to use a HashMap to store the path the user
+/// supplied while using the canonicalized path as the key.
+#[derive(Debug, Eq)]
+struct FilePath {
+    // The path that the user supplied
+    path: String,
+    // The canonicalized path
+    canonicalized_path: PathBuf,
+}
+
+impl TryFrom<String> for FilePath {
+    type Error = io::Error;
+
+    /// Creates a new [FilePath] from the given path. If the path does not exist, an [Error] is returned.
+    #[allow(clippy::result_large_err)]
+    fn try_from(path: String) -> Result<Self, Self::Error> {
+        PathBuf::from(&path).canonicalize().map(|canonicalized_path| Self {
+            path,
+            canonicalized_path,
+        })
+    }
+}
+
+impl Hash for FilePath {
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        self.canonicalized_path.hash(state);
+    }
+}
+
+impl PartialEq for FilePath {
+    fn eq(&self, other: &Self) -> bool {
+        self.canonicalized_path == other.canonicalized_path
+    }
+}
 
 pub fn resolve_files_from(options: &SliceOptions, diagnostic_reporter: &mut DiagnosticReporter) -> Vec<SliceFile> {
     // Create a map of all the Slice files with entries like: (absolute_path, is_source).
     // HashMap protects against files being passed twice (as reference and source).
     // It's important to add sources AFTER references, so sources overwrite references and not vice versa.
     let mut file_paths = HashMap::new();
-    file_paths.extend(find_slice_files(&options.references).into_iter().map(|f| (f, false)));
-    file_paths.extend(find_slice_files(&options.sources).into_iter().map(|f| (f, true)));
+
+    file_paths.extend(
+        find_slice_files(&options.references)
+            .into_iter()
+            .filter_map(|f| match FilePath::try_from(f) {
+                Ok(file_path) => Some(file_path),
+                Err(error) => {
+                    Error::new(ErrorKind::IO { error }).report(diagnostic_reporter);
+                    None
+                }
+            })
+            .map(|f| (f, false)),
+    );
+
+    file_paths.extend(
+        find_slice_files(&options.sources)
+            .into_iter()
+            .filter_map(|f| match FilePath::try_from(f) {
+                Ok(file_path) => Some(file_path),
+                Err(error) => {
+                    Error::new(ErrorKind::IO { error }).report(diagnostic_reporter);
+                    None
+                }
+            })
+            .map(|f| (f, true)),
+    );
 
     // Iterate through the discovered files and try to read them into Strings.
     // Report an error if it fails, otherwise create a new `SliceFile` to hold the data.
     let mut files = Vec::new();
     for (file_path, is_source) in file_paths {
-        match fs::read_to_string(&file_path) {
-            Ok(raw_text) => files.push(SliceFile::new(file_path, raw_text, is_source)),
+        match fs::read_to_string(&file_path.path) {
+            Ok(raw_text) => files.push(SliceFile::new(file_path.path, raw_text, is_source)),
             Err(error) => Error::new(ErrorKind::IO { error }).report(diagnostic_reporter),
         }
     }
+
     files
 }
 
---
+++ tests/files/io.rs
@@ -0,0 +1,47 @@
+// Copyright (c) ZeroC, Inc. All rights reserved.
+
+use std::path::{PathBuf, MAIN_SEPARATOR};
+
+use slice::command_line::SliceOptions;
+use slice::diagnostics::DiagnosticReporter;
+use slice::utils::file_util::resolve_files_from;
+
+#[test]
+fn duplicate_reference_files_ignored() {
+    // Arrange
+    let file = PathBuf::from(["tests", "files", "..", "files", "test.slice"].join(&MAIN_SEPARATOR.to_string()));
+    let options = SliceOptions {
+        sources: vec![file.to_str().unwrap().to_owned()],
+        references: vec![file.to_str().unwrap().to_owned()],
+        ..Default::default()
+    };
+    let mut reporter = DiagnosticReporter::new(&options);
+
+    // Act
+    let files = resolve_files_from(&options, &mut reporter);
+
+    // Assert
+    assert_eq!(files.len(), 1);
+}
+
+#[test]
+fn duplicate_source_files_ignored() {
+    // Arrange
+    let file_path_one = PathBuf::from(["tests", "files", "test.slice"].join(&MAIN_SEPARATOR.to_string()));
+    let file_path_two =
+        PathBuf::from(["tests", "files", "..", "files", "test.slice"].join(&MAIN_SEPARATOR.to_string()));
+    let options = SliceOptions {
+        sources: vec![
+            file_path_one.to_str().unwrap().to_owned(),
+            file_path_two.to_str().unwrap().to_owned(),
+        ],
+        ..Default::default()
+    };
+    let mut reporter = DiagnosticReporter::new(&options);
+
+    // Act
+    let files = resolve_files_from(&options, &mut reporter);
+
+    // Assert
+    assert_eq!(files.len(), 1);
+}
