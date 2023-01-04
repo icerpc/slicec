ee628e3ac52b3fb20473d56e77bd39ab947d3ea7
Joe George <joe@zeroc.com>
2023-01-04 14:07:04 -0500

    Improvements to IO error reporting (#396)
    - Add action and path to ErrorKind::IO
    - Report io errors when reading all source and reference files.
      Previously many errors where just sent to stderr
    - Fixes #392

--- src/utils/file_util.rs
+++ src/utils/file_util.rs
@@ -18,14 +18,14 @@ struct FilePath {
     canonicalized_path: PathBuf,
 }
 
-impl TryFrom<String> for FilePath {
+impl TryFrom<&String> for FilePath {
     type Error = io::Error;
 
     /// Creates a new [FilePath] from the given path. If the path does not exist, an [Error] is returned.
     #[allow(clippy::result_large_err)]
-    fn try_from(path: String) -> Result<Self, Self::Error> {
+    fn try_from(path: &String) -> Result<Self, Self::Error> {
         PathBuf::from(&path).canonicalize().map(|canonicalized_path| Self {
-            path,
+            path: path.clone(),
             canonicalized_path,
         })
     }
@@ -49,31 +49,8 @@ pub fn resolve_files_from(options: &SliceOptions, diagnostic_reporter: &mut Diag
     // It's important to add sources AFTER references, so sources overwrite references and not vice versa.
     let mut file_paths = HashMap::new();
 
-    file_paths.extend(
-        find_slice_files(&options.references)
-            .into_iter()
-            .filter_map(|f| match FilePath::try_from(f) {
-                Ok(file_path) => Some(file_path),
-                Err(error) => {
-                    Error::new(ErrorKind::IO { error }).report(diagnostic_reporter);
-                    None
-                }
-            })
-            .map(|f| (f, false)),
-    );
-
-    file_paths.extend(
-        find_slice_files(&options.sources)
-            .into_iter()
-            .filter_map(|f| match FilePath::try_from(f) {
-                Ok(file_path) => Some(file_path),
-                Err(error) => {
-                    Error::new(ErrorKind::IO { error }).report(diagnostic_reporter);
-                    None
-                }
-            })
-            .map(|f| (f, true)),
-    );
+    file_paths.extend(find_slice_files(&options.references, false, diagnostic_reporter));
+    file_paths.extend(find_slice_files(&options.sources, true, diagnostic_reporter));
 
     // Iterate through the discovered files and try to read them into Strings.
     // Report an error if it fails, otherwise create a new `SliceFile` to hold the data.
@@ -81,32 +58,59 @@ pub fn resolve_files_from(options: &SliceOptions, diagnostic_reporter: &mut Diag
     for (file_path, is_source) in file_paths {
         match fs::read_to_string(&file_path.path) {
             Ok(raw_text) => files.push(SliceFile::new(file_path.path, raw_text, is_source)),
-            Err(error) => Error::new(ErrorKind::IO { error }).report(diagnostic_reporter),
+            Err(error) => Error::new(ErrorKind::IO {
+                action: "read",
+                path: file_path.path,
+                error,
+            })
+            .report(diagnostic_reporter),
         }
     }
 
     files
 }
 
-fn find_slice_files(paths: &[String]) -> Vec<String> {
+fn find_slice_files(
+    paths: &[String],
+    source_files: bool,
+    diagnostic_reporter: &mut DiagnosticReporter,
+) -> Vec<(FilePath, bool)> {
     let mut slice_paths = Vec::new();
     for path in paths {
-        match find_slice_files_in_path(PathBuf::from(path)) {
+        match find_slice_files_in_path(PathBuf::from(path), diagnostic_reporter) {
             Ok(child_paths) => slice_paths.extend(child_paths),
-            Err(err) => eprintln!("failed to read file '{path}': {err}"),
+            Err(error) => Error::new(ErrorKind::IO {
+                action: "read",
+                path: path.to_owned(),
+                error,
+            })
+            .report(diagnostic_reporter),
         }
     }
 
     slice_paths
         .into_iter()
-        .map(|path| path.to_str().unwrap().to_owned())
+        .map(|path| path.display().to_string())
+        .filter_map(|path| match FilePath::try_from(&path) {
+            Ok(file_path) => Some(file_path),
+            Err(error) => {
+                Error::new(ErrorKind::IO {
+                    action: "read",
+                    path,
+                    error,
+                })
+                .report(diagnostic_reporter);
+                None
+            }
+        })
+        .map(|f| (f, source_files))
         .collect()
 }
 
-fn find_slice_files_in_path(path: PathBuf) -> io::Result<Vec<PathBuf>> {
+fn find_slice_files_in_path(path: PathBuf, diagnostic_reporter: &mut DiagnosticReporter) -> io::Result<Vec<PathBuf>> {
     // If the path is a directory, recursively search it for more slice files.
     if fs::metadata(&path)?.is_dir() {
-        find_slice_files_in_directory(path.read_dir()?)
+        find_slice_files_in_directory(path.read_dir()?, diagnostic_reporter)
     }
     // If the path is not a directory, check if it ends with 'slice'.
     else if path.extension().filter(|ext| ext.to_str() == Some("slice")).is_some() {
@@ -116,13 +120,21 @@ fn find_slice_files_in_path(path: PathBuf) -> io::Result<Vec<PathBuf>> {
     }
 }
 
-fn find_slice_files_in_directory(dir: fs::ReadDir) -> io::Result<Vec<PathBuf>> {
+fn find_slice_files_in_directory(
+    dir: fs::ReadDir,
+    diagnostic_reporter: &mut DiagnosticReporter,
+) -> io::Result<Vec<PathBuf>> {
     let mut paths = Vec::new();
     for child in dir {
         let child_path = child?.path();
-        match find_slice_files_in_path(child_path.clone()) {
+        match find_slice_files_in_path(child_path.clone(), diagnostic_reporter) {
             Ok(child_paths) => paths.extend(child_paths),
-            Err(err) => eprintln!("failed to read file '{}': {err}", child_path.display()),
+            Err(error) => Error::new(ErrorKind::IO {
+                action: "read",
+                path: child_path.display().to_string(),
+                error,
+            })
+            .report(diagnostic_reporter),
         }
     }
     Ok(paths)
