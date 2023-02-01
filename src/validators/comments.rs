5a5ecedc0bd5d1d834be4197de9ef14985587167
Austin Henriksen <austin@zeroc.com>
2023-02-01 10:52:17 -0500

    We patch comment links, so we don't need a validator for this anymore.

--- src/validators/comments.rs
+++ src/validators/comments.rs
@@ -1,7 +1,6 @@
 // Copyright (c) ZeroC, Inc. All rights reserved.
 
-use crate::ast::Ast;
-use crate::diagnostics::{DiagnosticReporter, ErrorKind, Warning, WarningKind};
+use crate::diagnostics::{DiagnosticReporter, Warning, WarningKind};
 use crate::grammar::*;
 use crate::validators::{ValidationChain, Validator};
 
@@ -10,7 +9,6 @@ pub fn comments_validators() -> ValidationChain {
         Validator::Entities(only_operations_can_throw),
         Validator::Operations(non_empty_return_comment),
         Validator::Operations(missing_parameter_comment),
-        Validator::DocComments(linked_identifiers_exist),
     ]
 }
 
@@ -63,53 +61,5 @@ fn only_operations_can_throw(entity: &dyn Entity, diagnostic_reporter: &mut Diag
                 .report(diagnostic_reporter)
             }
         };
-    }
-}
-
-macro_rules! check_link {
-    ($tag:expr, $entity:expr, $ast:expr, $diagnostic_reporter:expr) => {
-        if let Err(error) = $ast.find_element_with_scope::<dyn Entity>(&$tag.link.value, $entity.module_scope()) {
-            let message = match error.kind() {
-                ErrorKind::DoesNotExist { identifier } => {
-                    format!("no element with identifier '{identifier}' can be found from this scope.")
-                }
-                ErrorKind::TypeMismatch { actual, .. } => {
-                    format!("elements of type '{actual}' cannot be referenced in doc comments.")
-                }
-                _ => unreachable!(), // No other types of errors can be thrown from `find_element_with_scope`
-            };
-            Warning::new(WarningKind::InvalidDocCommentLinkIdentifier { message })
-                .set_span(&$tag.span)
-                .set_scope($entity.parser_scoped_identifier())
-                .report($diagnostic_reporter);
-        }
-    };
-}
-
-fn linked_identifiers_exist(entity: &dyn Entity, ast: &Ast, diagnostic_reporter: &mut DiagnosticReporter) {
-    let mut check_links_in = |message: &Message| {
-        for component in message {
-            if let MessageComponent::Link(link_tag) = component {
-                check_link!(link_tag, entity, ast, diagnostic_reporter);
-            }
-        }
-    };
-
-    if let Some(comment) = entity.comment() {
-        if let Some(overview) = &comment.overview {
-            check_links_in(&overview.message);
-        }
-        for param_tag in &comment.params {
-            check_links_in(&param_tag.message);
-        }
-        for returns_tag in &comment.returns {
-            check_links_in(&returns_tag.message);
-        }
-        for throws_tag in &comment.throws {
-            check_links_in(&throws_tag.message);
-        }
-        for see_tag in &comment.see {
-            check_link!(see_tag, entity, ast, diagnostic_reporter);
-        }
     }
 }
