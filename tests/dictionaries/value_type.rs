4508d3260782e0e4831f440580600d9aa2f461aa
Reece Humphreys <reece@zeroc.com>
2023-02-15 14:04:08 -0500

    Validate dictionary values (#445)

---
+++ tests/dictionaries/value_type.rs
@@ -0,0 +1,31 @@
+// Copyright (c) ZeroC, Inc.
+
+use crate::assert_errors;
+use crate::helpers::parsing_helpers::parse_for_diagnostics;
+use slice::diagnostics::{Error, ErrorKind};
+
+#[test]
+fn invalid_dictionary_values_produce_error() {
+    // Arrange
+    let slice = "
+    module Foo;
+    struct Bar {
+         i: dictionary<int32, dictionary<float32, bool>>,
+         j: dictionary<int32, sequence<dictionary<float64, bool>>>,
+    }
+    ";
+
+    // Act
+    let diagnostics = parse_for_diagnostics(slice);
+
+    // Assert
+    let expected = vec![
+        Error::new(ErrorKind::KeyTypeNotSupported {
+            identifier: "float32".to_owned(),
+        }),
+        Error::new(ErrorKind::KeyTypeNotSupported {
+            identifier: "float64".to_owned(),
+        }),
+    ];
+    assert_errors!(diagnostics, expected);
+}
