5baffa1d873cc164ab8f55bfdd2b585acebaddf6
Austin Henriksen <austin@zeroc.com>
2023-03-06 10:46:32 -0500

    Fixed formatting.

--- tests/typealias_tests.rs
+++ tests/typealias_tests.rs
@@ -127,15 +127,15 @@ mod typealias {
 
         // Assert
         let expected = Error::new(ErrorKind::OptionalTypeAlias)
-        .set_span(&Span::new((3, 13).into(), (3, 27).into(), "string-0"))
-        .add_note(
-            "try removing the trailing `?` modifier from its definition",
-            Some(&Span::new((3, 30).into(), (3, 35).into(), "string-0")),
-        )
-        .add_note(
-            "instead of defining an optional alias, try making it optional wherever you use it",
-            None,
-        );
+            .set_span(&Span::new((3, 13).into(), (3, 27).into(), "string-0"))
+            .add_note(
+                "try removing the trailing `?` modifier from its definition",
+                Some(&Span::new((3, 30).into(), (3, 35).into(), "string-0")),
+            )
+            .add_note(
+                "instead of defining an optional alias, try making it optional wherever you use it",
+                None,
+            );
 
         check_diagnostics(diagnostics, [expected]);
     }
