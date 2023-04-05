4d7e014c7d7ae681058e359d87da7c59e72b84ba
Austin Henriksen <austin@zeroc.com>
2023-04-05 14:54:02 -0400

    Dramatically simplified sequence and dictionary validation. (#508)

--- src/validators/sequence.rs
+++ src/validators/sequence.rs
@@ -1,25 +1,8 @@
 // Copyright (c) ZeroC, Inc.
 
 use crate::diagnostics::DiagnosticReporter;
-use crate::grammar::*;
-use crate::validators::dictionary::{has_allowed_key_type, has_allowed_value_type};
-use crate::validators::{ValidationChain, Validator};
+use crate::grammar::Sequence;
 
-pub fn sequence_validators() -> ValidationChain {
-    vec![Validator::Sequences(has_allowed_contents)]
-}
-
-fn has_allowed_contents(sequences: &[&Sequence], diagnostic_reporter: &mut DiagnosticReporter) {
-    sequences.iter().for_each(|s| validate_contents(s, diagnostic_reporter));
-}
-
-fn validate_contents(sequence: &Sequence, diagnostic_reporter: &mut DiagnosticReporter) {
-    match sequence.element_type.concrete_type() {
-        Types::Dictionary(dictionary) => {
-            has_allowed_key_type(&[dictionary], diagnostic_reporter);
-            has_allowed_value_type(&[dictionary], diagnostic_reporter);
-        }
-        Types::Sequence(sequence) => validate_contents(sequence, diagnostic_reporter),
-        _ => {}
-    }
+pub fn validate(sequence: &Sequence, diagnostic_reporter: &mut DiagnosticReporter) {
+    super::validate_type_ref(&sequence.element_type, diagnostic_reporter);
 }
