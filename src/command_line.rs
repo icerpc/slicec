1e193a8a13300fbc3a9086dc54f2bbc88545a483
Joe George <joe@zeroc.com>
2023-04-21 13:44:48 -0400

    Review fixes

--- src/command_line.rs
+++ src/command_line.rs
@@ -18,36 +18,36 @@ pub struct SliceOptions {
 
     /// Reference Slice file or directory containing Slice files. Reference files are used to resolve definitions in
     /// the Slice sources being compiled. Directories are searched recursively. This option can be repeated.
-    #[arg(short = 'R', long = "reference", num_args = 1, action = Append)]
+    #[arg(short = 'R', value_name="REFERENCE", num_args = 1, action = Append)]
     pub references: Vec<String>,
 
-    /// Define a preprocessor definition. Preprocessor definitions do not have an associated value.
+    /// Define a preprocessor definition. Preprocessor definitions do not have associated values.
     /// This option can be repeated.
-    #[arg(short = 'D', long = "define", num_args = 1, action = Append)]
+    #[arg(short = 'D', value_name="DEFINITION", num_args = 1, action = Append)]
     pub definitions: Vec<String>,
 
     /// Instruct the compiler to treat warnings as errors.
-    #[arg(short, long)]
+    #[arg(short = 'W')]
     pub warn_as_error: bool,
 
     /// Instruct the compiler to allow the specified warning. An allowed warning will not be emitted as a
     /// diagnostic. This option can be repeated.
-    #[arg(short = 'A', long = "allow")]
+    #[arg(short = 'A', long = "allow", value_name="WARNING", num_args = 1, action = Append)]
     pub allowed_warnings: Option<Vec<String>>,
 
     /// Validate input files without generating code for them.
     #[arg(long)]
     pub dry_run: bool,
 
-    /// Set the output directory for the generated code, defaults to the current working directory.
-    #[arg(long)]
+    /// Set the output directory for the generated code. Defaults to the current working directory.
+    #[arg(short = 'O', long)]
     pub output_dir: Option<String>,
 
     /// Output format for emitted errors.
     #[arg(value_enum, default_value_t = DiagnosticFormat::Human, long, ignore_case = true)]
     pub diagnostic_format: DiagnosticFormat,
 
-    /// Disable ANSI escape code for diagnostic output.
+    /// Disable ANSI color codes in diagnostic output.
     #[arg(long)]
     pub disable_color: bool,
 }
