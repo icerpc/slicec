1d74ddc472c883eef3ab25fcc97a28b8f1942a70
Joe George <joe@zeroc.com>
2023-05-16 15:23:19 -0400

    Review fixes

--- src/validators/attribute.rs
+++ src/validators/attribute.rs
@@ -1,10 +1,7 @@
 // Copyright (c) ZeroC, Inc.
 
 use crate::diagnostics::{Diagnostic, DiagnosticReporter, Error};
-use crate::grammar::module::Module;
-use crate::grammar::r#struct::Struct;
-use crate::grammar::type_ref::TypeRef;
-use crate::grammar::{Attributable, Attribute, AttributeKind, Symbol};
+use crate::grammar::{Attributable, Attribute, AttributeKind, Exception, Module, Struct, Symbol, TypeRef};
 use crate::slice_file::SliceFile;
 use crate::visitor::Visitor;
 use std::collections::hash_map::Entry::{Occupied, Vacant};
@@ -17,49 +14,49 @@ pub struct AttributeValidator<'a> {
 
 impl Visitor for AttributeValidator<'_> {
     fn visit_file(&mut self, slice_file: &SliceFile) {
-        let attributes = &slice_file.attributes(false);
-        validate_repeated_attributes(attributes, self.diagnostic_reporter);
-        for attribute in attributes {
+        let attributes = slice_file.attributes(false);
+        validate_repeated_attributes(&attributes, self.diagnostic_reporter);
+        for attribute in &attributes {
             validate_common_attributes(attribute, self.diagnostic_reporter)
         }
     }
 
     fn visit_module(&mut self, module_def: &Module) {
-        let attributes = &module_def.attributes(false);
-        validate_repeated_attributes(attributes, self.diagnostic_reporter);
-        for attribute in attributes {
+        let attributes = module_def.attributes(false);
+        validate_repeated_attributes(&attributes, self.diagnostic_reporter);
+        for attribute in &attributes {
             validate_common_attributes(attribute, self.diagnostic_reporter)
         }
     }
 
     fn visit_struct(&mut self, struct_def: &Struct) {
-        let attributes = &struct_def.attributes(false);
-        validate_repeated_attributes(attributes, self.diagnostic_reporter);
-        for attribute in attributes {
+        let attributes = struct_def.attributes(false);
+        validate_repeated_attributes(&attributes, self.diagnostic_reporter);
+        for attribute in &attributes {
             validate_common_attributes(attribute, self.diagnostic_reporter)
         }
     }
 
     fn visit_class(&mut self, class_def: &crate::grammar::class::Class) {
-        let attributes = &class_def.attributes(false);
-        validate_repeated_attributes(attributes, self.diagnostic_reporter);
-        for attribute in attributes {
+        let attributes = class_def.attributes(false);
+        validate_repeated_attributes(&attributes, self.diagnostic_reporter);
+        for attribute in &attributes {
             validate_common_attributes(attribute, self.diagnostic_reporter)
         }
     }
 
-    fn visit_exception(&mut self, exception_def: &crate::grammar::exception::Exception) {
-        let attributes = &exception_def.attributes(false);
-        validate_repeated_attributes(attributes, self.diagnostic_reporter);
-        for attribute in attributes {
+    fn visit_exception(&mut self, exception_def: &Exception) {
+        let attributes = exception_def.attributes(false);
+        validate_repeated_attributes(&attributes, self.diagnostic_reporter);
+        for attribute in &attributes {
             validate_common_attributes(attribute, self.diagnostic_reporter)
         }
     }
 
     fn visit_interface(&mut self, interface_def: &crate::grammar::interface::Interface) {
-        let attributes = &interface_def.attributes(false);
-        validate_repeated_attributes(attributes, self.diagnostic_reporter);
-        for attribute in attributes {
+        let attributes = interface_def.attributes(false);
+        validate_repeated_attributes(&attributes, self.diagnostic_reporter);
+        for attribute in &attributes {
             match attribute.kind {
                 AttributeKind::Compress { .. } => {}
                 _ => validate_common_attributes(attribute, self.diagnostic_reporter),
@@ -68,17 +65,17 @@ impl Visitor for AttributeValidator<'_> {
     }
 
     fn visit_enum(&mut self, enum_def: &crate::grammar::r#enum::Enum) {
-        let attributes = &enum_def.attributes(false);
-        validate_repeated_attributes(attributes, self.diagnostic_reporter);
-        for attribute in attributes {
+        let attributes = enum_def.attributes(false);
+        validate_repeated_attributes(&attributes, self.diagnostic_reporter);
+        for attribute in &attributes {
             validate_common_attributes(attribute, self.diagnostic_reporter)
         }
     }
 
     fn visit_operation(&mut self, operation: &crate::grammar::operation::Operation) {
-        let attributes = &operation.attributes(false);
-        validate_repeated_attributes(attributes, self.diagnostic_reporter);
-        for attribute in attributes {
+        let attributes = operation.attributes(false);
+        validate_repeated_attributes(&attributes, self.diagnostic_reporter);
+        for attribute in &attributes {
             match attribute.kind {
                 AttributeKind::Compress { .. } => {}
                 AttributeKind::Oneway { .. } => {}
@@ -89,33 +86,33 @@ impl Visitor for AttributeValidator<'_> {
     }
 
     fn visit_custom_type(&mut self, custom_type: &crate::grammar::custom_type::CustomType) {
-        let attributes = &custom_type.attributes(false);
-        validate_repeated_attributes(attributes, self.diagnostic_reporter);
-        for attribute in attributes {
+        let attributes = custom_type.attributes(false);
+        validate_repeated_attributes(&attributes, self.diagnostic_reporter);
+        for attribute in &attributes {
             validate_common_attributes(attribute, self.diagnostic_reporter)
         }
     }
 
     fn visit_type_alias(&mut self, type_alias: &crate::grammar::type_alias::TypeAlias) {
-        let attributes = &type_alias.attributes(false);
-        validate_repeated_attributes(attributes, self.diagnostic_reporter);
-        for attribute in attributes {
+        let attributes = type_alias.attributes(false);
+        validate_repeated_attributes(&attributes, self.diagnostic_reporter);
+        for attribute in &attributes {
             validate_common_attributes(attribute, self.diagnostic_reporter)
         }
     }
 
     fn visit_field(&mut self, field: &crate::grammar::field::Field) {
-        let attributes = &field.attributes(false);
-        validate_repeated_attributes(attributes, self.diagnostic_reporter);
-        for attribute in attributes {
+        let attributes = field.attributes(false);
+        validate_repeated_attributes(&attributes, self.diagnostic_reporter);
+        for attribute in &attributes {
             validate_common_attributes(attribute, self.diagnostic_reporter)
         }
     }
 
     fn visit_parameter(&mut self, parameter: &crate::grammar::parameter::Parameter) {
-        let attributes = &parameter.attributes(false);
-        validate_repeated_attributes(attributes, self.diagnostic_reporter);
-        for attribute in attributes {
+        let attributes = parameter.attributes(false);
+        validate_repeated_attributes(&attributes, self.diagnostic_reporter);
+        for attribute in &attributes {
             match attribute.kind {
                 // Issue an error here since deprecated is allowed everywhere else
                 AttributeKind::Deprecated { .. } => {
@@ -132,16 +129,16 @@ impl Visitor for AttributeValidator<'_> {
     }
 
     fn visit_enumerator(&mut self, enumerator: &crate::grammar::enumerator::Enumerator) {
-        let attributes = &enumerator.attributes(false);
-        validate_repeated_attributes(attributes, self.diagnostic_reporter);
-        for attribute in attributes {
+        let attributes = enumerator.attributes(false);
+        validate_repeated_attributes(&attributes, self.diagnostic_reporter);
+        for attribute in &attributes {
             validate_common_attributes(attribute, self.diagnostic_reporter)
         }
     }
 
     fn visit_type_ref(&mut self, type_ref: &TypeRef) {
-        let attributes = &type_ref.attributes(false);
-        for attribute in attributes {
+        let attributes = type_ref.attributes(false);
+        for attribute in &attributes {
             match attribute.kind {
                 AttributeKind::LanguageKind { .. } => {}
                 AttributeKind::Other { .. } => {}
