8d24fa37c90500e24f97b2f96231315747547bcd
Austin Henriksen <austin@zeroc.com>
2023-05-22 16:02:29 -0400

    Simplified 'parse_file'.

--- src/parsers/mod.rs
+++ src/parsers/mod.rs
@@ -14,53 +14,39 @@ mod slice;
 
 use crate::ast::Ast;
 use crate::compilation_state::CompilationState;
-use crate::diagnostics::{Diagnostic, DiagnosticReporter};
+use crate::diagnostics::Diagnostic;
 use crate::slice_file::SliceFile;
 use std::collections::HashSet;
 
 pub fn parse_files(state: &mut CompilationState, symbols: &HashSet<String>) {
     for file in state.files.values_mut() {
-        parse_file(file, &mut state.ast, &mut state.diagnostic_reporter, symbols.clone());
+        // Attempt to parse the file.
+        let mut diagnostics = Vec::new();
+        parse_file(file, &mut state.ast, &mut diagnostics, symbols.clone());
+
+        // Forward any diagnostics that were emitted during parsing to the diagnostic reporter.
+        for diagnostic in diagnostics {
+            diagnostic.report(&mut state.diagnostic_reporter);
+        }
     }
 }
 
-fn parse_file(
-    file: &mut SliceFile,
-    ast: &mut Ast,
-    diagnostic_reporter: &mut DiagnosticReporter,
-    symbols: HashSet<String>,
-) {
-    // Attempt to parse the file.
-    let mut diagnostics = Vec::new();
-    let _ = try_parse_file(file, ast, &mut diagnostics, symbols);
-
-    // Forward any diagnostics that were emitted during parsing to the diagnostic reporter.
-    for diagnostic in diagnostics {
-        diagnostic.report(diagnostic_reporter);
-    }
-}
-
-fn try_parse_file(
-    file: &mut SliceFile,
-    ast: &mut Ast,
-    diagnostics: &mut Vec<Diagnostic>,
-    mut symbols: HashSet<String>,
-) -> common::ParserResult<()> {
+fn parse_file(file: &mut SliceFile, ast: &mut Ast, diagnostics: &mut Vec<Diagnostic>, mut symbols: HashSet<String>) {
     // Pre-process the file's raw text.
     let preprocessor = Preprocessor::new(&file.relative_path, &mut symbols, diagnostics);
-    let preprocessed_text = preprocessor.parse_slice_file(file.raw_text.as_str())?;
+    let Ok(preprocessed_text) = preprocessor.parse_slice_file(file.raw_text.as_str()) else { return; };
 
     // If no text remains after pre-processing, the file is empty and we can skip parsing and exit early.
     // To check the length of the preprocessed text without consuming the iterator we convert it to a peekable iterator,
     // then check the peek value.
     let mut peekable_preprocessed_text = preprocessed_text.peekable();
     if peekable_preprocessed_text.peek().is_none() {
-        return Err(());
+        return;
     }
 
     // Parse the preprocessed text.
     let parser = Parser::new(&file.relative_path, ast, diagnostics);
-    let (file_encoding, attributes, modules) = parser.parse_slice_file(peekable_preprocessed_text)?;
+    let Ok((file_encoding, attributes, modules)) = parser.parse_slice_file(peekable_preprocessed_text) else { return; };
 
     // Add the top-level-modules into the AST, but keep `WeakPtr`s to them.
     let top_level_modules = modules
@@ -72,5 +58,4 @@ fn try_parse_file(
     file.encoding = file_encoding;
     file.attributes = attributes;
     file.contents = top_level_modules;
-    Ok(())
 }
