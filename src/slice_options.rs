4c1c21224ea9f6c22b8ba2482a82e9f82ca4f70e
Austin Henriksen <austin@zeroc.com>
2023-06-26 14:52:55 -0400

    Remove 'warn-as-error- CLI flag.' (#618)

--- src/slice_options.rs
+++ src/slice_options.rs
@@ -24,10 +24,6 @@ pub struct SliceOptions {
     #[arg(short = 'D', value_name="SYMBOL", num_args = 1, action = Append)]
     pub defined_symbols: Vec<String>,
 
-    /// Instruct the compiler to treat warnings as errors.
-    #[arg(short = 'W', long)]
-    pub warn_as_error: bool,
-
     /// Instruct the compiler to allow (not emit) the specified warning.
     #[arg(short = 'A', long = "allow", value_name="WARNING", num_args = 1, action = Append)]
     pub allowed_warnings: Vec<String>,
