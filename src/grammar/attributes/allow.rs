b54210ba9f6658c9ab96a0b149979d71e2edd9f9
Austin Henriksen <austin@zeroc.com>
2023-06-27 09:59:01 -0400

    Let Clap parse arguments to the 'allow' CLI option. (#619)

--- src/grammar/attributes/allow.rs
+++ src/grammar/attributes/allow.rs
@@ -12,7 +12,36 @@ impl Allow {
         debug_assert_eq!(directive, Self::directive());
 
         check_that_arguments_were_provided(args, Self::directive(), span, reporter);
-        validate_allow_arguments(args, Some(span), reporter);
+
+        for arg in args {
+            let mut is_valid = Warning::ALLOWABLE_WARNING_IDENTIFIERS.contains(&arg.as_str());
+
+            // The `DuplicateFile` lint can't be configured by attributes because it's a command-line specific warning.
+            if arg == "DuplicateFile" {
+                is_valid = false;
+            }
+
+            // Emit an error if the argument wasn't valid.
+            if !is_valid {
+                // TODO we should emit a link to the warnings page when we write it!
+                let mut error = Diagnostic::new(Error::ArgumentNotSupported {
+                    argument: arg.to_owned(),
+                    directive: "allow".to_owned(),
+                })
+                .set_span(span);
+
+                // Check if the argument only differs in case from a valid one.
+                let suggestion = Warning::ALLOWABLE_WARNING_IDENTIFIERS
+                    .iter()
+                    .find(|identifier| identifier.eq_ignore_ascii_case(arg));
+                if let Some(identifier) = suggestion {
+                    let message = format!("attribute arguments are case sensitive, perhaps you meant '{identifier}'?");
+                    error = error.add_note(message, None);
+                }
+
+                error.report(reporter);
+            }
+        }
 
         let allowed_warnings = args.clone();
         Allow { allowed_warnings }
@@ -26,41 +55,3 @@ impl Allow {
 }
 
 implement_attribute_kind_for!(Allow, "allow", true);
-
-// This is a standalone function because it's used by both the `allow` attribute, and the `--allow` CLI option.
-pub fn validate_allow_arguments(arguments: &[String], span: Option<&Span>, reporter: &mut DiagnosticReporter) {
-    for argument in arguments {
-        let argument_str = &argument.as_str();
-        let mut is_valid = Warning::ALLOWABLE_WARNING_IDENTIFIERS.contains(argument_str);
-
-        // We don't allow `DuplicateFile` to be suppressed by attributes, because it's a command-line specific warning.
-        // This check works because `span` is `None` for command line flags.
-        if argument == "DuplicateFile" && span.is_some() {
-            is_valid = false;
-        }
-
-        // Emit an error if the argument wasn't valid.
-        if !is_valid {
-            // TODO we should emit a link to the warnings page when we write it!
-            let mut error = Diagnostic::new(Error::ArgumentNotSupported {
-                argument: argument.to_owned(),
-                directive: "allow".to_owned(),
-            });
-
-            if let Some(unwrapped_span) = span {
-                error = error.set_span(unwrapped_span);
-            }
-
-            // Check if the argument only differs in case from a valid one.
-            let suggestion = Warning::ALLOWABLE_WARNING_IDENTIFIERS
-                .iter()
-                .find(|identifier| identifier.eq_ignore_ascii_case(argument_str));
-            if let Some(identifier) = suggestion {
-                let message = format!("attribute arguments are case sensitive, perhaps you meant '{identifier}'?");
-                error = error.add_note(message, None);
-            }
-
-            error.report(reporter);
-        }
-    }
-}
