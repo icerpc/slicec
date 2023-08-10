e3f1499f16fb2c98e01d30fb55725e1b31d6a526
Austin Henriksen <austin@zeroc.com>
2023-08-10 01:43:57 -0400

    Remove another double 'that' typo.

--- src/parsers/comments/mod.rs
+++ src/parsers/comments/mod.rs
@@ -13,7 +13,7 @@ type ParseError<'a> = lalrpop_util::ParseError<Location, TokenKind<'a>, Error<'a
 
 // TODO add more specific error messages for common cases.
 
-/// Converts an [error](Error) that was emitted from the parser/lexer into a [lint](Lint) that
+/// Converts an [error](Error) that was emitted from the parser/lexer into a [lint](Lint)
 /// that can be stored in a [`Diagnostics`](crate::diagnostics::Diagnostics) struct.
 fn construct_lint_from(parse_error: ParseError, file_name: &str) -> Diagnostic {
     match parse_error {
