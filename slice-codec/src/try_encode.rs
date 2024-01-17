98141f5e843a22ce0655bc7bb87f0fb7d564b13b
Austin Henriksen <austin@zeroc.com>
2024-01-17 10:12:59 -0500

    Removed default error handlers from encode/decode functions.

--- slice-codec/src/try_encode.rs
+++ slice-codec/src/try_encode.rs
@@ -7,28 +7,12 @@ pub trait TryEncode<E: Encoding>
     where Self: Sized,
 {
     fn try_encode(self, encoder: &mut Encoder<E>) -> Result<()>;
-
-    fn encode(self, encoder: &mut Encoder<E>) {
-        default_error_handler(self.try_encode(encoder))
-    }
 }
 
 pub trait TryEncodeCollection<E: Encoding, T>
     where Self: Sized,
 {
     fn try_encode_with_fn(self, encoder: &mut Encoder<E>, encode_fn: EncodeFn<T, E>) -> Result<()>;
-
-    fn encode_with_fn(self, encoder: &mut Encoder<E>, encode_fn: EncodeFn<T, E>) {
-        default_error_handler(self.try_encode_with_fn(encoder, encode_fn))
-    }
 }
 
 pub type EncodeFn<T, E> = fn(T, &mut Encoder<E>) -> Result<()>;
-
-#[inline(always)]
-fn default_error_handler<T>(result: Result<T>) -> T {
-    match result {
-        Ok(value) => value,
-        Err(error) => panic!("failed to encode\n{error:?}"),
-    }
-}
