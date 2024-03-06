356e19bc263e82d9c65de3d9e0f718528eae4b31
Austin Henriksen <austin@zeroc.com>
2024-03-06 12:17:12 -0500

    Added better spacing to build-all script for readability.

--- slice-codec/build-all.bat
+++ slice-codec/build-all.bat
@@ -8,137 +8,159 @@ REM This line can be enabled and disabled to treat warnings as errors.
 REM SET RUSTFLAGS=-Dwarnings
 
 cargo build --no-default-features
-if %ERRORLEVEL% NEQ 0 EXIT ELSE ECHO.
+if %ERRORLEVEL% NEQ 0 EXIT
 cargo build --no-default-features --features alloc
-if %ERRORLEVEL% NEQ 0 EXIT ELSE ECHO.
+if %ERRORLEVEL% NEQ 0 EXIT
 cargo build --no-default-features --features std
-if %ERRORLEVEL% NEQ 0 EXIT ELSE ECHO.
+if %ERRORLEVEL% NEQ 0 EXIT
 cargo build --no-default-features --features bytes
-if %ERRORLEVEL% NEQ 0 EXIT ELSE ECHO.
+if %ERRORLEVEL% NEQ 0 EXIT
 cargo build --no-default-features --features std,bytes
-if %ERRORLEVEL% NEQ 0 EXIT ELSE ECHO.
+if %ERRORLEVEL% NEQ 0 EXIT
+
+ECHO.
 
 cargo build --no-default-features --features slice2
-if %ERRORLEVEL% NEQ 0 EXIT ELSE ECHO.
+if %ERRORLEVEL% NEQ 0 EXIT
 cargo build --no-default-features --features slice2,alloc
-if %ERRORLEVEL% NEQ 0 EXIT ELSE ECHO.
+if %ERRORLEVEL% NEQ 0 EXIT
 cargo build --no-default-features --features slice2,std
-if %ERRORLEVEL% NEQ 0 EXIT ELSE ECHO.
+if %ERRORLEVEL% NEQ 0 EXIT
 cargo build --no-default-features --features slice2,bytes
-if %ERRORLEVEL% NEQ 0 EXIT ELSE ECHO.
+if %ERRORLEVEL% NEQ 0 EXIT
 cargo build --no-default-features --features slice2,std,bytes
-if %ERRORLEVEL% NEQ 0 EXIT ELSE ECHO.
+if %ERRORLEVEL% NEQ 0 EXIT
+
+ECHO.
 
 cargo build --no-default-features --features slice1
-if %ERRORLEVEL% NEQ 0 EXIT ELSE ECHO.
+if %ERRORLEVEL% NEQ 0 EXIT
 cargo build --no-default-features --features slice1,alloc
-if %ERRORLEVEL% NEQ 0 EXIT ELSE ECHO.
+if %ERRORLEVEL% NEQ 0 EXIT
 cargo build --no-default-features --features slice1,std
-if %ERRORLEVEL% NEQ 0 EXIT ELSE ECHO.
+if %ERRORLEVEL% NEQ 0 EXIT
 cargo build --no-default-features --features slice1,bytes
-if %ERRORLEVEL% NEQ 0 EXIT ELSE ECHO.
+if %ERRORLEVEL% NEQ 0 EXIT
 cargo build --no-default-features --features slice1,std,bytes
-if %ERRORLEVEL% NEQ 0 EXIT ELSE ECHO.
+if %ERRORLEVEL% NEQ 0 EXIT
+
+ECHO.
 
 cargo build --no-default-features --features slice1,slice2
-if %ERRORLEVEL% NEQ 0 EXIT ELSE ECHO.
+if %ERRORLEVEL% NEQ 0 EXIT
 cargo build --no-default-features --features slice1,slice2,alloc
-if %ERRORLEVEL% NEQ 0 EXIT ELSE ECHO.
+if %ERRORLEVEL% NEQ 0 EXIT
 cargo build --no-default-features --features slice1,slice2,std
-if %ERRORLEVEL% NEQ 0 EXIT ELSE ECHO.
+if %ERRORLEVEL% NEQ 0 EXIT
 cargo build --no-default-features --features slice1,slice2,bytes
-if %ERRORLEVEL% NEQ 0 EXIT ELSE ECHO.
+if %ERRORLEVEL% NEQ 0 EXIT
 cargo build --no-default-features --features slice1,slice2,std,bytes
-if %ERRORLEVEL% NEQ 0 EXIT ELSE ECHO.
+if %ERRORLEVEL% NEQ 0 EXIT
 
-
+ECHO.
+ECHO.
+ECHO.
 
 cargo clippy --all-targets --no-default-features
-if %ERRORLEVEL% NEQ 0 EXIT ELSE ECHO.
+if %ERRORLEVEL% NEQ 0 EXIT
 cargo clippy --all-targets --no-default-features --features alloc
-if %ERRORLEVEL% NEQ 0 EXIT ELSE ECHO.
+if %ERRORLEVEL% NEQ 0 EXIT
 cargo clippy --all-targets --no-default-features --features std
-if %ERRORLEVEL% NEQ 0 EXIT ELSE ECHO.
+if %ERRORLEVEL% NEQ 0 EXIT
 cargo clippy --all-targets --no-default-features --features bytes
-if %ERRORLEVEL% NEQ 0 EXIT ELSE ECHO.
+if %ERRORLEVEL% NEQ 0 EXIT
 cargo clippy --all-targets --no-default-features --features std,bytes
-if %ERRORLEVEL% NEQ 0 EXIT ELSE ECHO.
+if %ERRORLEVEL% NEQ 0 EXIT
+
+ECHO.
 
 cargo clippy --all-targets --no-default-features --features slice2
-if %ERRORLEVEL% NEQ 0 EXIT ELSE ECHO.
+if %ERRORLEVEL% NEQ 0 EXIT
 cargo clippy --all-targets --no-default-features --features slice2,alloc
-if %ERRORLEVEL% NEQ 0 EXIT ELSE ECHO.
+if %ERRORLEVEL% NEQ 0 EXIT
 cargo clippy --all-targets --no-default-features --features slice2,std
-if %ERRORLEVEL% NEQ 0 EXIT ELSE ECHO.
+if %ERRORLEVEL% NEQ 0 EXIT
 cargo clippy --all-targets --no-default-features --features slice2,bytes
-if %ERRORLEVEL% NEQ 0 EXIT ELSE ECHO.
+if %ERRORLEVEL% NEQ 0 EXIT
 cargo clippy --all-targets --no-default-features --features slice2,std,bytes
-if %ERRORLEVEL% NEQ 0 EXIT ELSE ECHO.
+if %ERRORLEVEL% NEQ 0 EXIT
+
+ECHO.
 
 cargo clippy --all-targets --no-default-features --features slice1
-if %ERRORLEVEL% NEQ 0 EXIT ELSE ECHO.
+if %ERRORLEVEL% NEQ 0 EXIT
 cargo clippy --all-targets --no-default-features --features slice1,alloc
-if %ERRORLEVEL% NEQ 0 EXIT ELSE ECHO.
+if %ERRORLEVEL% NEQ 0 EXIT
 cargo clippy --all-targets --no-default-features --features slice1,std
-if %ERRORLEVEL% NEQ 0 EXIT ELSE ECHO.
+if %ERRORLEVEL% NEQ 0 EXIT
 cargo clippy --all-targets --no-default-features --features slice1,bytes
-if %ERRORLEVEL% NEQ 0 EXIT ELSE ECHO.
+if %ERRORLEVEL% NEQ 0 EXIT
 cargo clippy --all-targets --no-default-features --features slice1,std,bytes
-if %ERRORLEVEL% NEQ 0 EXIT ELSE ECHO.
+if %ERRORLEVEL% NEQ 0 EXIT
+
+ECHO.
 
 cargo clippy --all-targets --no-default-features --features slice1,slice2
-if %ERRORLEVEL% NEQ 0 EXIT ELSE ECHO.
+if %ERRORLEVEL% NEQ 0 EXIT
 cargo clippy --all-targets --no-default-features --features slice1,slice2,alloc
-if %ERRORLEVEL% NEQ 0 EXIT ELSE ECHO.
+if %ERRORLEVEL% NEQ 0 EXIT
 cargo clippy --all-targets --no-default-features --features slice1,slice2,std
-if %ERRORLEVEL% NEQ 0 EXIT ELSE ECHO.
+if %ERRORLEVEL% NEQ 0 EXIT
 cargo clippy --all-targets --no-default-features --features slice1,slice2,bytes
-if %ERRORLEVEL% NEQ 0 EXIT ELSE ECHO.
+if %ERRORLEVEL% NEQ 0 EXIT
 cargo clippy --all-targets --no-default-features --features slice1,slice2,std,bytes
-if %ERRORLEVEL% NEQ 0 EXIT ELSE ECHO.
+if %ERRORLEVEL% NEQ 0 EXIT
 
-
+ECHO.
+ECHO.
+ECHO.
 
 cargo test --no-default-features
-if %ERRORLEVEL% NEQ 0 EXIT ELSE ECHO.
+if %ERRORLEVEL% NEQ 0 EXIT
 cargo test --no-default-features --features alloc
-if %ERRORLEVEL% NEQ 0 EXIT ELSE ECHO.
+if %ERRORLEVEL% NEQ 0 EXIT
 cargo test --no-default-features --features std
-if %ERRORLEVEL% NEQ 0 EXIT ELSE ECHO.
+if %ERRORLEVEL% NEQ 0 EXIT
 cargo test --no-default-features --features bytes
-if %ERRORLEVEL% NEQ 0 EXIT ELSE ECHO.
+if %ERRORLEVEL% NEQ 0 EXIT
 cargo test --no-default-features --features std,bytes
-if %ERRORLEVEL% NEQ 0 EXIT ELSE ECHO.
+if %ERRORLEVEL% NEQ 0 EXIT
+
+ECHO.
 
 cargo test --no-default-features --features slice2
-if %ERRORLEVEL% NEQ 0 EXIT ELSE ECHO.
+if %ERRORLEVEL% NEQ 0 EXIT
 cargo test --no-default-features --features slice2,alloc
-if %ERRORLEVEL% NEQ 0 EXIT ELSE ECHO.
+if %ERRORLEVEL% NEQ 0 EXIT
 cargo test --no-default-features --features slice2,std
-if %ERRORLEVEL% NEQ 0 EXIT ELSE ECHO.
+if %ERRORLEVEL% NEQ 0 EXIT
 cargo test --no-default-features --features slice2,bytes
-if %ERRORLEVEL% NEQ 0 EXIT ELSE ECHO.
+if %ERRORLEVEL% NEQ 0 EXIT
 cargo test --no-default-features --features slice2,std,bytes
-if %ERRORLEVEL% NEQ 0 EXIT ELSE ECHO.
+if %ERRORLEVEL% NEQ 0 EXIT
+
+ECHO.
 
 cargo test --no-default-features --features slice1
-if %ERRORLEVEL% NEQ 0 EXIT ELSE ECHO.
+if %ERRORLEVEL% NEQ 0 EXIT
 cargo test --no-default-features --features slice1,alloc
-if %ERRORLEVEL% NEQ 0 EXIT ELSE ECHO.
+if %ERRORLEVEL% NEQ 0 EXIT
 cargo test --no-default-features --features slice1,std
-if %ERRORLEVEL% NEQ 0 EXIT ELSE ECHO.
+if %ERRORLEVEL% NEQ 0 EXIT
 cargo test --no-default-features --features slice1,bytes
-if %ERRORLEVEL% NEQ 0 EXIT ELSE ECHO.
+if %ERRORLEVEL% NEQ 0 EXIT
 cargo test --no-default-features --features slice1,std,bytes
-if %ERRORLEVEL% NEQ 0 EXIT ELSE ECHO.
+if %ERRORLEVEL% NEQ 0 EXIT
+
+ECHO.
 
 cargo test --no-default-features --features slice1,slice2
-if %ERRORLEVEL% NEQ 0 EXIT ELSE ECHO.
+if %ERRORLEVEL% NEQ 0 EXIT
 cargo test --no-default-features --features slice1,slice2,alloc
-if %ERRORLEVEL% NEQ 0 EXIT ELSE ECHO.
+if %ERRORLEVEL% NEQ 0 EXIT
 cargo test --no-default-features --features slice1,slice2,std
-if %ERRORLEVEL% NEQ 0 EXIT ELSE ECHO.
+if %ERRORLEVEL% NEQ 0 EXIT
 cargo test --no-default-features --features slice1,slice2,bytes
-if %ERRORLEVEL% NEQ 0 EXIT ELSE ECHO.
+if %ERRORLEVEL% NEQ 0 EXIT
 cargo test --no-default-features --features slice1,slice2,std,bytes
-if %ERRORLEVEL% NEQ 0 EXIT ELSE ECHO.
+if %ERRORLEVEL% NEQ 0 EXIT
