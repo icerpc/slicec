a82548a9aa6d057a4726c3447b1d879e8cc284f7
Austin Henriksen <austin@zeroc.com>
2024-04-23 09:45:03 -0400

    Move buffer 'From' impls to a private module that is 'cfg(slice2)'.

--- slice-codec/src/buffer/mod.rs
+++ slice-codec/src/buffer/mod.rs
@@ -2,43 +2,47 @@
 
 //! TODO maybe write a comment explaining this module?
 
-use self::input::SliceInputSource;
-use self::output::{SliceOutputTarget, VecOutputTarget};
-use crate::decoder::Decoder;
-use crate::encoder::Encoder;
-
 pub mod input;
 pub mod output;
 
-// Allows users to create an [`Encoder`] directly from a slice,
-// without needing to construct an intermediate [`SliceOutputTarget`].
 #[cfg(feature = "slice2")]
-impl<'a, T> From<T> for Encoder<SliceOutputTarget<'a>>
-    where T: Into<SliceOutputTarget<'a>>,
-{
-    fn from(value: T) -> Self {
-        Encoder::new_with_inferred_encoding(value.into())
-    }
-}
+mod slice2_from_impls {
+    use super::input::SliceInputSource;
+    use super::output::SliceOutputTarget;
+    use crate::decoder::Decoder;
+    use crate::encoder::Encoder;
 
-// Allows users to create an [`Encoder`] directly from a vector,
-// without needing to construct an intermediate [`VecOutputTarget`].
-#[cfg(all(feature = "alloc", feature = "slice2"))]
-impl<'a, T> From<T> for Encoder<VecOutputTarget<'a>>
-    where T: Into<VecOutputTarget<'a>>,
-{
-    fn from(value: T) -> Self {
-        Encoder::new_with_inferred_encoding(value.into())
-    }
-}
+    #[cfg(feature = "alloc")]
+    use super::output::VecOutputTarget;
 
-// Allows users to create a [`Decoder`] directly from a slice,
-// without needing to construct an intermediate [`SliceInputSource`].
-#[cfg(feature = "slice2")]
-impl<'a, T> From<T> for Decoder<SliceInputSource<'a>>
-    where T: Into<SliceInputSource<'a>>,
-{
-    fn from(value: T) -> Self {
-        Decoder::new_with_inferred_encoding(value.into())
+    // Allows users to create an [`Encoder`] directly from a slice,
+    // without needing to construct an intermediate [`SliceOutputTarget`].
+    impl<'a, T> From<T> for Encoder<SliceOutputTarget<'a>>
+        where T: Into<SliceOutputTarget<'a>>,
+    {
+        fn from(value: T) -> Self {
+            Encoder::new_with_inferred_encoding(value.into())
+        }
+    }
+
+    // Allows users to create an [`Encoder`] directly from a vector,
+    // without needing to construct an intermediate [`VecOutputTarget`].
+    #[cfg(feature = "alloc")]
+    impl<'a, T> From<T> for Encoder<VecOutputTarget<'a>>
+        where T: Into<VecOutputTarget<'a>>,
+    {
+        fn from(value: T) -> Self {
+            Encoder::new_with_inferred_encoding(value.into())
+        }
+    }
+
+    // Allows users to create a [`Decoder`] directly from a slice,
+    // without needing to construct an intermediate [`SliceInputSource`].
+    impl<'a, T> From<T> for Decoder<SliceInputSource<'a>>
+        where T: Into<SliceInputSource<'a>>,
+    {
+        fn from(value: T) -> Self {
+            Decoder::new_with_inferred_encoding(value.into())
+        }
     }
 }
