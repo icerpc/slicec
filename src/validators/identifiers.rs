0902d3e99924f0b6bff838a570079e866378c5eb
Austin Henriksen <austin@zeroc.com>
2024-09-10 10:24:51 -0400

    No, this doesn't quite work either...

--- src/validators/identifiers.rs
+++ src/validators/identifiers.rs
@@ -3,6 +3,7 @@
 use crate::ast::Ast;
 use crate::diagnostics::{Diagnostic, Diagnostics, Error};
 use crate::grammar::*;
+use crate::visitor::Visitor;
 use std::collections::HashMap;
 
 pub fn validate_inherited_identifiers(
@@ -42,52 +43,77 @@ fn check_for_shadowing(
 }
 
 pub fn check_for_redefinitions(ast: &Ast, diagnostics: &mut Diagnostics) {
-    // A map storing `(scoped_identifier, named_symbol)` pairs. We iterate through the AST and build up this map,
-    // and if we try to add an entry but see it's already occupied, that means we've found a redefinition.
-    let mut slice_definitions: HashMap<String, &dyn NamedSymbol> = HashMap::new();
+    for node in ast.as_slice() {
 
-    // A list of all the 'scoped_identifier's we've issued redefinition errors for.
-    // This is to avoid emitting errors for children of redefined containers.
-    // NOTE: These entries must all end in "::" to avoid false matches between things like "foo" and "foobar".
-    let mut redefinitions: Vec<String> = Vec::new();
-
-    // Iterate through the nodes in reverse. This is important because our parser is 'bottom-up', meaning we create
-    // fields before we create the struct that holds them. So, by iterating in reverse, we'll see the containers before
-    // we see their children elements.
-    for node in ast.as_slice().iter().rev() {
-        // Only check nodes that have identifiers, everything else is irrelevant.
-        if let Ok(definition) = <&dyn NamedSymbol>::try_from(node) {
-            let scoped_identifier = definition.parser_scoped_identifier();
-            match slice_definitions.get(&scoped_identifier) {
-                // If we've already seen a node with this identifier, there's a name collision.
-                // This is fine for modules (since they can be re-opened), but for any other type, we report an error.
+            // Check if we've already seen another entity with this exact identifier.
+            match seen_definitions.get(&scoped_identifier) {
+                // If there is, we've found a name collision.
                 Some(other_definition) => {
-                    let are_both_modules = is_module(definition) && is_module(*other_definition);
-                    let parent_is_redefined = redefinitions.iter().any(|redefined_parent_id| {
-                        scoped_identifier.contains(redefined_parent_id) && scoped_identifier != *redefined_parent_id
-                    });
-
-                    if !are_both_modules && !parent_is_redefined {
+                    // The last thing to check is that these entities both have the same parent element.
+                    // Otherwise we can hit false positives where redefining a container can trigger spurious
+                    // redefinition errors for it's contents (since the scope segments are still the same).
+                    if are_in_the_same_container(definition, *other_definition) {
                         report_redefinition_error(definition, *other_definition, diagnostics);
-                        redefinitions.push(scoped_identifier + "::");
                     }
                 }
 
-                // If we haven't seen a node with this identifier before, add it to the map and continue checking.
+                // If we haven't seen this identifier before, add it to the map and continue checking.
                 None => {
-                    slice_definitions.insert(scoped_identifier, definition);
+                    seen_definitions.insert(scoped_identifier, definition);
                 }
             }
         }
     }
 }
 
-// TODO improve this function.
-fn is_module(definition: &dyn NamedSymbol) -> bool {
-    definition.kind() == "module"
+/// Returns true if `definition` and `other` are in the same container, or if they're both at module level.
+///
+/// Note that when they're both at module level, we don't make sure that this is the same module!
+/// This function is only called from `check_for_redefinitions` which already guarantees this.
+fn are_in_the_same_container(definition: &dyn Entity, other: &dyn Entity) -> bool {
+    eprintln!("Here we are:\n{definition:?}\n{other:?}");
+
+    match definition.concrete_entity() {
+        Entities::Field(field) => {
+            eprintln!("We made it here at least");
+            let x = match other.concrete_entity() {
+                Entities::Field(other_field) => {
+                    eprintln!("=== what the heck?? {}", field.parent == other_field.parent());
+                    field.parent == other_field.parent()
+                }
+                _ => false, // if they aren't the same type, they can't be in the same container.
+            };
+            eprintln!("{x}");
+            x
+        }
+        Entities::Enumerator(enumerator) => {
+            match other.concrete_entity() {
+                Entities::Enumerator(other_enumerator) => enumerator.parent == other_enumerator.parent(),
+                _ => false, // if they aren't the same type, they can't be in the same container.
+            }
+        }
+        Entities::Operation(operation) => {
+            match other.concrete_entity() {
+                Entities::Operation(other_operation) => operation.parent == &other_operation.parent(),
+                _ => false, // if they aren't the same type, they can't be in the same container.
+            }
+        }
+        Entities::Parameter(parameter) => {
+            match other.concrete_entity() {
+                Entities::Parameter(other_parameter) => parameter.parent == &other_parameter.parent(),
+                _ => false, // if they aren't the same type, they can't be in the same container.
+            }
+        }
+
+        // Otherwise the definition is at module level, all we have to do is make sure the other one is too.
+        _ => !matches!(
+            other.concrete_entity(),
+            Entities::Field(_) | Entities::Enumerator(_) | Entities::Operation(_) | Entities::Parameter(_)
+        ),
+    }
 }
 
-fn report_redefinition_error(new: &dyn NamedSymbol, original: &dyn NamedSymbol, diagnostics: &mut Diagnostics) {
+fn report_redefinition_error(new: &dyn Entity, original: &dyn Entity, diagnostics: &mut Diagnostics) {
     Diagnostic::new(Error::Redefinition {
         identifier: new.identifier().to_owned(),
     })
